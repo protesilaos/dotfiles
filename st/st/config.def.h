61e0cedd1057c69fe353d3dbf378a3ccf8e5043e
Protesilaos Stavrou <info@protesilaos.com>
2019-02-12 13:44:14 +0200

    Sync with my ST fork. Font size should be 9.5
    See the comments in this commit: https://gitlab.com/protesilaos/st/commit/85452cfe7dbd9886b9069e2a9f88877df7bce595

--- st/st/config.def.h
+++ st/st/config.def.h
@@ -22,7 +22,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:size=10";
+static char *font = "monospace:size=9.5";
 static int borderpx = 0;
 
 /*
