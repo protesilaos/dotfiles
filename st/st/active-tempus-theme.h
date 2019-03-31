306a0ee2c0ca2ba9be51266de6c759f8e38f23eb
Protesilaos Stavrou <info@protesilaos.com>
2019-03-31 23:31:55 +0300

    BREAKING: Remove Simple Terminal source code
    This is because I am deprecating ST as my default terminal emulator in
    favour of MATE Terminal.  For a detailed report, see this commit:
    ba4f7dd330c13a2194fb6b907cb32cfa4958e1d0

--- st/st/active-tempus-theme.h
+++
@@ -1,7 +0,0 @@
-/* vi: ft=c
- * IMPORTANT do not edit manually!!!
- * All themes (Xcolors, URxvt, bspwm, etc.) are changed by a single
- * script.  See my `~/bin/own_script_update_environment_theme`.  Part of
- * my dotfiles: https://gitlab.com/protesilaos/dotfiles.
- */
-#include "tempus_classic.h"
