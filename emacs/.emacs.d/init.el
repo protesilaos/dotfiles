24b189f5b1288c97fbf2df3bd11a48226a55a5ea
Protesilaos Stavrou <info@protesilaos.com>
2019-11-12 10:33:11 +0200

    MERGE Emacs
    The previous repo of my Emacs setup is now DEPRECATED:
    https://gitlab.com/protesilaos/dotemacs
    
    Everything is part of https://gitlab.com/protesilaos/dotfiles.
    
    The web page of my Emacs init file will always be:
    https://protesilaos.com/dotemacs

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,41 @@
+;; init.el --- Personal GNU Emacs configuration file.
+
+;; Copyright (c) 2019 Protesilaos Stavrou <info@protesilaos.com>
+
+;; This file is not part of GNU Emacs.  Its source online is:
+;; https://gitlab.com/protesilaos/dotemacs
+
+;;; License:
+
+;; This file is free software: you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the
+;; Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file sets up the essentials for incorporating my init org
+;; file.  This is known as "literate programming", which I think is
+;; particularly helpful for sharing Emacs configurations with a wider
+;; audience that includes new or potential users (I am still very new
+;; myself).
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+			 '("melpa" . "https://melpa.org/packages/"))
+(unless package--initialized (package-initialize))
+(setq use-package-compute-statistics t)
+
+(require 'org)
+(org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
+
+;; init.el ends here
