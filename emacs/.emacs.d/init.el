1e35f592091516e32d58284f3fe9681e00aa64a6
Protesilaos Stavrou <info@protesilaos.com>
2020-01-13 17:37:18 +0200

    Emacs 27: simplify `init.el'

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -31,10 +31,9 @@
 ;;; Code:
 
 (require 'package)
-(setq package-enable-at-startup nil)
+
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
-(unless package--initialized (package-initialize))
 
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/emacs-init.org"))
