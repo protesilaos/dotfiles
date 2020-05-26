4cae86c04b15a874ebc3995d13b4e55b3eb7bca1
Protesilaos Stavrou <info@protesilaos.com>
2020-05-26 18:01:06 +0300

    Emacs: tentative tweaks for package-quickstart
    This will likely require further testing.

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -32,5 +32,8 @@
 ;; Do not initialise the package manager.  This is done in `init.el'.
 (setq package-enable-at-startup nil)
 
+;; Allow loading from the package cache.
+(setq package-quickstart t)
+
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
