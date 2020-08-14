b736087dcf87d4357dbced86c01ba624113052b3
Protesilaos Stavrou <info@protesilaos.com>
2020-08-14 09:26:32 +0300

    Emacs modus themes: sync latest updates
    For documentation, screen shots, proper commit and change logs, refer to
    their repo: https://gitlab.com/protesilaos/modus-themes

--- emacs/.emacs.d/modus-vivendi-theme.el
+++ emacs/.emacs.d/modus-vivendi-theme.el
@@ -258,6 +258,7 @@
 ;;     perspective
 ;;     phi-grep
 ;;     phi-search
+;;     pkgbuild-mode
 ;;     pomidor
 ;;     powerline
 ;;     powerline-evil
@@ -1080,6 +1081,7 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(comint-highlight-prompt ((,class ,@(modus-vivendi-theme-bold-weight) :foreground ,cyan-alt-other)))
    `(error ((,class :inherit bold :foreground ,red)))
    `(escape-glyph ((,class :foreground ,fg-escape-char-construct)))
+   `(file-name-shadow ((,class :foreground ,fg-unfocused)))
    `(header-line ((,class :background ,bg-header :foreground ,fg-header)))
    `(header-line-highlight ((,class :inherit modus-theme-active-blue)))
    `(homoglyph ((,class :foreground ,fg-escape-char-construct)))
@@ -3207,6 +3209,8 @@ Also bind `class' to ((class color) (min-colors 89))."
    `(phi-search-failpart-face ((,class :inherit modus-theme-refine-red)))
    `(phi-search-match-face ((,class :inherit modus-theme-refine-cyan)))
    `(phi-search-selection-face ((,class :inherit (modus-theme-intense-green bold))))
+;;;;; pkgbuild-mode
+   `(pkgbuild-error-face ((,class :underline (:color ,fg-lang-error))))
 ;;;;; pomidor
    `(pomidor-break-face ((,class :foreground ,blue-alt-other)))
    `(pomidor-overwork-face ((,class :foreground ,red-alt-other)))
