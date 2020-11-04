1e125a6263ba67b82b814136a7ddad0fad5a24a6
Protesilaos Stavrou <info@protesilaos.com>
2020-11-04 21:13:31 +0200

    Emacs USLS: fix malformed paths in usls-find-file

--- emacs/.emacs.d/lisp/usls.el
+++ emacs/.emacs.d/lisp/usls.el
@@ -263,7 +263,7 @@ If the region is active, append it to the newly created file."
   (interactive)
   (let* ((files (usls--directory-files))
          (file (completing-read "Visit file: " files nil t nil 'usls--link-history))
-         (item (concat (file-truename (concat usls-directory file)))))
+         (item (file-truename file)))
     (find-file item)
     (add-to-history 'usls--link-history item)))
 
