44850fdfd0ccf1188544f745439ad3464b704064
Protesilaos Stavrou <info@protesilaos.com>
2021-01-09 15:49:37 +0200

    Emacs: minor early-init.el tweaks

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -29,11 +29,19 @@
 
 ;;; Code:
 
-;; Initialise installed packages
-(setq package-enable-at-startup t)
+;; Do not initialise installed packages (I use `straight.el')
+(setq package-enable-at-startup nil)
 
-;; Allow loading from the package cache.
-(setq package-quickstart t)
+;; Do not allow loading from the package cache (same reason).
+(setq package-quickstart nil)
 
 ;; Do not resize the frame at this early stage.
 (setq frame-inhibit-implied-resize t)
+
+;; Disable GUI elements
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq inhibit-splash-screen t)
+(setq use-dialog-box t)               ; only for mouse events
+(setq use-file-dialog nil)
