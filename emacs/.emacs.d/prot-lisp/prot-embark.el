79348353145c9ef544d90fc549e4a5d54c768057
Protesilaos Stavrou <info@protesilaos.com>
2021-04-10 09:34:36 +0300

    prot-emacs: del prot-embark-extras; update Embark

--- emacs/.emacs.d/prot-lisp/prot-embark.el
+++ emacs/.emacs.d/prot-lisp/prot-embark.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://protesilaos.com/dotemacs
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "28.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
@@ -28,15 +28,10 @@
 ;; <https://protesilaos.com/dotemacs/>.
 ;;
 ;; NOTE 2021-04-02: Everything pertaining to the completions' buffer has
-;; been moved to `prot-minibuffer.el'.  What I have here about that
-;; niche was useful because I was using Embark's live updating
-;; completions' collection buffer.  However, Emacs28 provides a
-;; one-column layout for the default Completions' buffer, so it is easy
-;; to move that code there and adapt it to work without the otherwise
-;; minor Embark extras.
+;; been moved to `prot-minibuffer.el'.
 ;;
-;; As such, most of this file's contents are kept here for posterity
-;; and/or are subject to review/removal.
+;; NOTE 2021-04-10: What once was `prot-embark-extras.el' has been
+;; merged here.
 ;;
 ;; Remember that every piece of Elisp that I write is for my own
 ;; educational and recreational purposes.  I am not a programmer and I
@@ -55,302 +50,106 @@
   "Extensions for `embark'."
   :group 'editing)
 
-;;;###autoload
-(defun prot-embark-clear-live-buffers ()
-  "Remove lingering Embark Collect Completions' buffers.
-Add this to `minibuffer-exit-hook'."
-  (let* ((buffers (buffer-list))
-         (case-fold-search nil)
-         (completions
-          (cl-remove-if-not (lambda (buf)
-                              (string-match "\\*Embark.*Completions.*"
-                                            (format "%s" buf)))
-                            buffers)))
-    (mapc #'kill-buffer completions)))
+;;;; Extra keymaps
 
-;; Thanks to Omar Antolín Camarena for providing a variant of this!
-;; (mistakes are always my own).
-;;;###autoload
-(defun prot-embark-collect-fit-window (&rest _)
-  "Fit Embark's live occur window to its buffer.
-To be added to `embark-occur-post-revert-hook'."
-  (when (derived-mode-p 'embark-collect-mode)
-    (fit-window-to-buffer (get-buffer-window)
-                          (floor (frame-height) 2) 1)))
+(autoload 'prot-consult-fd "prot-consult")
+(autoload 'consult-grep "consult")
+(autoload 'consult-line "consult")
+(autoload 'consult-imenu "consult")
+(autoload 'consult-outline "consult")
 
-(defvar embark-collect-linked-buffer)
+(defvar prot-embark-become-general-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "f") 'prot-consult-fd)
+    (define-key map (kbd "g") 'consult-grep)
+    map)
+  "General custom cross-package `embark-become' keymap.")
 
-(defun prot-embark--live-buffer-p ()
-  "Determine presence of a linked live occur buffer."
-  (let ((buf embark-collect-linked-buffer))
-    (when buf
-      (window-live-p (get-buffer-window buf)))))
+(defvar prot-embark-become-line-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "l") 'consult-line)
+    (define-key map (kbd "i") 'consult-imenu)
+    (define-key map (kbd "s") 'consult-outline) ; as my default is 'M-s M-s'
+    map)
+  "Line-specific custom cross-package `embark-become' keymap.")
 
-(defvar embark-collect--kind)
+(defvar embark-become-file+buffer-map)
+(autoload 'prot-recentf-recent-files "prot-recentf")
+(autoload 'project-switch-to-buffer "project")
+(autoload 'project-find-file "project")
 
-;; Thanks to Omar Antolín Camarena for sharing this:
-;; <https://github.com/oantolin/embark/issues/114#issuecomment-761583395>
-(defun prot-embark--live-completions-p ()
-  "Determine whether current collection is for live completions."
-  (and (derived-mode-p 'embark-collect-mode)
-       (eq embark-collect--kind :completions)))
+(defvar prot-embark-become-file+buffer-map
+  (let ((map (make-sparse-keymap)))
+    (set-keymap-parent map embark-become-file+buffer-map)
+    (define-key map (kbd "r") 'prot-recentf-recent-files)
+    (define-key map (kbd "B") 'project-switch-to-buffer)
+    (define-key map (kbd "F") 'project-find-file)
+    map)
+  "File+buffer custom cross-package `embark-become' keymap.")
 
-(defface prot-embark-hl-line
-  '((default :extend t)
-    (((class color) (min-colors 88) (background light))
-     :background "#b0d8ff" :foreground "#000000")
-    (((class color) (min-colors 88) (background dark))
-     :background "#103265" :foreground "#ffffff")
-    (t :inherit (font-lock-string-face elfeed-search-title-face)))
-  "Face for current line in Embark completions."
-  :group 'prot-embark)
-
-(defface prot-embark-line-number
-  '((default :inherit default)
-    (((class color) (min-colors 88) (background light))
-     :background "#f2eff3" :foreground "#252525")
-    (((class color) (min-colors 88) (background dark))
-     :background "#151823" :foreground "#dddddd")
-    (t :inverse-video t))
-  "Face for line numbers in Embark completions."
-  :group 'prot-embark)
-
-(defface prot-embark-line-number-current-line
-  '((default :inherit default)
-    (((class color) (min-colors 88) (background light))
-     :background "#8ac7ff" :foreground "#000000")
-    (((class color) (min-colors 88) (background dark))
-     :background "#142a79" :foreground "#ffffff")
-    (t :inverse-video t))
-  "Face for current line number in Embark completions."
-  :group 'prot-embark)
-
-(autoload 'display-line-numbers-mode "display-line-numbers")
-(autoload 'face-remap-remove-relative "face-remap")
+(defvar embark-become-keymaps)
 
 ;;;###autoload
-(defun prot-embark-display-line-numbers ()
-  "Set up line numbers for live Embark collect buffers.
-Add this to `embark-collect-mode-hook'."
-  (if (prot-embark--live-completions-p)
-      (progn
-        (face-remap-add-relative 'line-number 'prot-embark-line-number)
-        (face-remap-add-relative 'line-number-current-line
-                                 'prot-embark-line-number-current-line)
-        (display-line-numbers-mode 1))
-    (display-line-numbers-mode -1)
-    ;; TODO: can we avoid `face-remap-add-relative' and just use the
-    ;; value it previously returned?
-    (face-remap-remove-relative
-     (face-remap-add-relative 'line-number
-                              'prot-embark-line-number))
-    (face-remap-remove-relative
-     (face-remap-add-relative 'line-number-current-line
-                              'prot-embark-line-number-current-line))))
+(define-minor-mode prot-embark-keymaps
+  "Add or remove keymaps from Embark.
+This is based on the value of `prot-embark-add-keymaps'
+and is meant to keep things clean in case I ever wish to disable
+those so-called 'extras'."
+  :init-value nil
+  :global t
+  (let ((maps (list 'prot-embark-become-general-map
+                    'prot-embark-become-line-map
+                    'prot-embark-become-file+buffer-map)))
+    (if prot-embark-keymaps
+        (dolist (map maps)
+          (cl-pushnew map embark-become-keymaps))
+      (setq embark-become-keymaps
+            (dolist (map maps)
+              (delete map embark-become-keymaps))))))
 
+;;;; Keycast integration
+
+;; Got this from Embark's wiki.  Renamed it to placate the compiler:
+;; <https://github.com/oantolin/embark/wiki/Additional-Configuration>.
+
+(defvar keycast--this-command-keys)
+(defvar keycast--this-command)
+
+(defun prot-embark--store-action-key+cmd (cmd)
+  "Configure keycast variables for keys and CMD.
+To be used as filter-return advice to `embark-keymap-prompter'."
+  (setq keycast--this-command-keys (this-single-command-keys)
+        keycast--this-command cmd))
+
+(advice-add 'embark-keymap-prompter :filter-return #'prot-embark--store-action-key+cmd)
+
+(defun prot-embark--force-keycast-update (&rest _)
+  "Update keycast's mode line.
+To be passed as advice before `embark-act' and others."
+  (force-mode-line-update t))
+
+(autoload 'embark-act "embark")
+(autoload 'embark-act-noexit "embark")
+(autoload 'embark-become "embark")
+
+;; NOTE: This has a generic name because my plan is to add more packages
+;; to it.
 ;;;###autoload
-(defun prot-embark-hl-line ()
-  "Set up line highlighting for live Embark collect buffers.
-Add this to `embark-collect-mode-hook'."
-  (if (prot-embark--live-completions-p)
-      (progn
-        (face-remap-add-relative 'hl-line 'prot-embark-hl-line)
-        (hl-line-mode 1))
-    (hl-line-mode -1)
-    ;; TODO: same as above with regard to `face-remap-add-relative'.
-    (face-remap-remove-relative
-     (face-remap-add-relative 'hl-line 'prot-embark-hl-line))))
+(define-minor-mode prot-embark-setup-packages
+  "Set up advice to integrate Embark with various commands."
+  :init-value nil
+  :global t
+  (if (and prot-embark-setup-packages
+           (require 'keycast nil t))
+      (dolist (cmd '(embark-act embark-become))
+        (advice-add cmd :before #'prot-embark--force-keycast-update))
+    (dolist (cmd '(embark-act embark-become))
+      (advice-remove cmd #'prot-embark--force-keycast-update))))
 
-;;;###autoload
-(defun prot-embark-completions-cursor ()
-  "`prot-minibuffer-completions-cursor' for Embark completions.
-Add this to `embark-collect-mode-hook'."
-  (if (prot-embark--live-completions-p)
-      (prot-minibuffer-completions-cursor) ; from `prot-minibuffer.el'
-    (kill-local-variable 'cursor-type)))
-
-(autoload 'embark-quit "embark")
-
-;; Thanks to Karthik Chikmagalur for providing an earlier version of
-;; `prot-embark-keyboard-quit' command!  Sources to Karthik's work:
-;;
-;; + https://karthinks.com/
-;; + https://github.com/karthink/.emacs.d/tree/clean
-;;
-;;;###autoload
-(defun prot-embark-keyboard-quit ()
-  "Control the exit behaviour for Embark collect buffers.
-
-If in a live Embark collect/completions buffer and unless the
-region is active, run `abort-recursive-edit'.  Otherwise run
-`keyboard-quit'.
-
-If the region is active, deactivate it.  A second invocation of
-this command is then required to abort the session.
-
-This is meant to be bound in `embark-collect-mode-map'."
-  (interactive)
-  (if (prot-embark--live-completions-p)
-      (if (use-region-p)
-          (keyboard-quit)
-        (kill-buffer)
-        (abort-recursive-edit))
-    (keyboard-quit)))
-
-(autoload 'embark-collect-completions "embark")
-
-;;;###autoload
-(defun prot-embark-completions-toggle ()
-  "Toggle `embark-collect-completions'."
-  (interactive)
-  (if (prot-embark--live-buffer-p)
-      (kill-buffer embark-collect-linked-buffer)
-    (embark-collect-completions)))
-
-(declare-function embark--act "embark")
-(declare-function embark--target "embark")
-(autoload 'embark-default-action "embark")
-
-(defun prot-embark--completions-act (arg)
-  "Move ARG lines and perform `embark-default-action'."
-  (forward-line arg)
-  (embark--act #'embark-default-action (cdr (embark--target))))
-
-;;;###autoload
-(defun prot-embark-completions-act-next (&optional arg)
-  "Run default action on next or ARGth Embark target.
-This calls `prot-embark--completions-act' and is meant to be
-assigned to a key in `embark-collect-mode-map'."
-  (interactive "p")
-  (prot-embark--completions-act (or arg 1)))
-
-;;;###autoload
-(defun prot-embark-completions-act-previous (&optional arg)
-  "Run default action on previous or ARGth Embark target.
-This calls `prot-embark--completions-act' and is meant to be
-assigned to a key in `embark-collect-mode-map'."
-  (interactive "p")
-  (let ((num (prot-common-number-negative arg))) ; from `prot-common.el'
-    (prot-embark--completions-act (or num -1))))
-
-;;;###autoload
-(defun prot-embark-completions-act-current ()
-  "Run default action on Embark target without exiting.
-Meant to be assigned to a key in `embark-collect-mode-map'."
-  (interactive)
-  (embark--act #'embark-default-action (cdr (embark--target))))
-
-(defun prot-embark--switch-to-completions ()
-  "Subroutine for switching to the Embark completions buffer."
-  (unless (prot-embark--live-buffer-p)
-    (prot-embark-completions-toggle))
-  (let ((win (get-buffer-window embark-collect-linked-buffer)))
-    (select-window win)))
-
-;;;###autoload
-(defun prot-embark-switch-to-completions-top ()
-  "Switch to the top of Embark's completions buffer.
-Meant to be bound in `minibuffer-local-completion-map'."
-  (interactive)
-  (prot-embark--switch-to-completions)
-  (goto-char (point-min)))
-
-;;;###autoload
-(defun prot-embark-switch-to-completions-bottom ()
-  "Switch to the bottom of Embark's completions buffer.
-Meant to be bound in `minibuffer-local-completion-map'."
-  (interactive)
-  (prot-embark--switch-to-completions)
-  (goto-char (point-max))
-  (forward-line -1)
-  (goto-char (point-at-bol))
-  (recenter
-   (- -1
-      (min (max 0 scroll-margin)
-           (truncate (/ (window-body-height) 4.0))))
-      t))
-
-;;;###autoload
-(defun prot-embark-next-line-or-mini (&optional arg)
-  "Move to the next line or switch to the minibuffer.
-This performs a regular motion for optional ARG lines, but when
-point can no longer move in that direction, then it switches to
-the minibuffer."
-  (interactive "p")
-  (if (or (eobp) (eq (point-max) (save-excursion (forward-line 1) (point))))
-      (prot-minibuffer-focus-minibuffer) ; from `prot-minibuffer.el'
-    (forward-line (or arg 1)))
-  (setq this-command 'next-line))
-
-;;;###autoload
-(defun prot-embark-previous-line-or-mini (&optional arg)
-  "Move to the next line or switch to the minibuffer.
-This performs a regular motion for optional ARG lines, but when
-point can no longer move in that direction, then it switches to
-the minibuffer."
-  (interactive "p")
-  (let ((num (prot-common-number-negative arg))) ; from `prot-common.el'
-    (if (bobp)
-        (prot-minibuffer-focus-minibuffer) ; from `prot-minibuffer.el'
-      (forward-line (or num 1)))))
-
-;;;###autoload
-(defun prot-embark-collection-kill-line ()
-  "Delete line from Embark collect buffer."
-  (interactive)
-  (let* ((inhibit-read-only t)
-         (eol (point-at-eol))
-         (eol-dwim (if (= eol (point-max)) eol (1+ eol))))
-    (save-excursion
-      (goto-char (point-at-bol))
-      (delete-region (point) eol-dwim))))
-
-;; NOTE 2021-02-06: De facto deprecated in favour of Consult's variants
-;;;###autoload
-(defun prot-embark-collection-flush-lines (regexp)
-  "`flush-lines' matching REGEXP in Embark collect buffers."
-  (interactive
-   (list (read-regexp "Flush lines matching regexp: ")))
-  (let ((inhibit-read-only t))
-    (if (derived-mode-p 'embark-collect-mode)
-        (with-current-buffer (current-buffer)
-            (save-excursion
-              (goto-char (point-min))
-              (flush-lines regexp)))
-      (user-error "Not in an Embark collect buffer"))))
-
-;; NOTE 2021-02-06: De facto deprecated in favour of Consult's variants
-;;;###autoload
-(defun prot-embark-collection-keep-lines (regexp)
-  "`keep-lines' matching REGEXP in Embark collect buffers."
-  (interactive
-   (list (read-regexp "Keep lines matching regexp: ")))
-  (let ((inhibit-read-only t))
-    (if (derived-mode-p 'embark-collect-mode)
-        (with-current-buffer (current-buffer)
-            (save-excursion
-              (goto-char (point-min))
-              (keep-lines regexp)))
-      (user-error "Not in an Embark collect buffer"))))
-
-(declare-function embark-consult-preview-minor-mode "embark-consult")
-(defvar embark-consult-preview-minor-mode)
-
-;;;###autoload
-(defun prot-embark-consult-preview-toggle ()
-  "Toggle preview mode for Embark's Consult collections."
-  (interactive)
-  (when (require 'embark-consult nil t)
-    (if (and (bound-and-true-p embark-consult-preview-minor-mode)
-             (derived-mode-p 'embark-collect-mode))
-        (progn
-          (remove-hook 'embark-collect-mode-hook #'embark-consult-preview-minor-mode)
-          (embark-consult-preview-minor-mode -1))
-      (add-hook 'embark-collect-mode-hook #'embark-consult-preview-minor-mode)
-      (embark-consult-preview-minor-mode 1))))
-
+;;;; which-key integration
 ;; NOTE: I keep this around for when I do videos, otherwise I do not use
-;; it.  It requires `which-key' to display key hints.
+;; it.
+
 (defvar embark-action-indicator)
 (defvar embark-become-indicator)
 (declare-function which-key--show-keymap "which-key")
