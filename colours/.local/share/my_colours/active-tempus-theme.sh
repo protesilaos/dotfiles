405403b5607aba386ad1bbc00373313f4d6f5214
Protesilaos Stavrou <info@protesilaos.com>
2021-08-27 10:08:51 +0300

    Delete obsolete "colours" target

--- colours/.local/share/my_colours/active-tempus-theme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Part of my dotfiles: https://gitlab.com/protesilaos/dotfiles
-#
-# IMPORTANT do not edit manually!!!  All themes (Xcolors, shell, bspwm,
-# etc.) are changed by a single script: tempus.
-
-source "$HOME"/.local/share/my_colours/shell/tempus_classic.sh
