164b9005ef65bd21593d899da6026ede68b8225e
Protesilaos Stavrou <info@protesilaos.com>
2021-12-09 12:09:28 +0200

    lin: sync latest updates
    Refer to its repo for documentation and proper commit/change logs:
    <https://gitlab.com/protesilaos/lin>.

--- emacs/.emacs.d/prot-lisp/lin.el
+++ emacs/.emacs.d/prot-lisp/lin.el
@@ -68,9 +68,11 @@
     ledger-report-mode-hook
     log-view-mode-hook
     magit-log-mode-hook
+    mu4e-headers-mode
     notmuch-search-mode-hook
     notmuch-tree-mode-hook
     org-agenda-mode-hook
+    ilist-mode-hook
     tabulated-list-mode-hook)
   "List of default hooks in which to use `lin-mode'.
 See also the user option `lin-override-foreground'.")
