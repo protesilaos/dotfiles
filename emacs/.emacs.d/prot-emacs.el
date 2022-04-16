5cc483fc55c9030b6c0a4e593eae6e79636e7762
Protesilaos Stavrou <info@protesilaos.com>
2022-04-16 09:58:38 +0300

    prot-emacs: sync .el version of configuration

--- emacs/.emacs.d/prot-emacs.el
+++ emacs/.emacs.d/prot-emacs.el
@@ -590,28 +590,11 @@
   ;; Those are the default values, but check the user option
   ;; `vertico-multiform-categories' for per-category tweaks.
   (setq vertico-scroll-margin 0)
-  (setq vertico-count 5)
+  (setq vertico-count 10)
   (setq vertico-resize nil)
   (setq vertico-cycle t)
-  (setq vertico-buffer-hide-prompt nil)
-  ;; NOTE: Do not conflate this with a side window.
-  (setq vertico-buffer-display-action
-        '(display-buffer-in-direction
-          (direction . leftmost)))
 
-  (vertico-multiform-mode 1)
   (vertico-mode 1)
-
-  (setq vertico-multiform-categories
-        '((consult-grep buffer)
-          (imenu buffer)
-          (consult-location buffer)
-          (command (vertico-count . 3) (vertico-resize . t))
-          (file grid (vertico-count . 10))
-          (project-file grid (vertico-count . 10))
-          (function buffer (vertico-sort-function . vertico-sort-alpha))
-          (symbol buffer (vertico-sort-function . vertico-sort-alpha))
-          (variable buffer (vertico-sort-function . vertico-sort-alpha))))
 
   ;; This works with `file-name-shadow-mode'.  When you are in a
   ;; sub-directory and use, say, `find-file' to go to your home '~/' or
@@ -2312,13 +2295,12 @@ sure this is a good approach."
 
 ;;; General UI
   (setq notmuch-show-logo nil)
-  (setq notmuch-column-control t)
+  (setq notmuch-column-control 1.0)
   (setq notmuch-hello-auto-refresh t)
   (setq notmuch-hello-recent-searches-max 20)
   (setq notmuch-hello-thousands-separator "")
-  ;; ;; See my variant of it in `prot-notmuch' below.
-  ;; (setq notmuch-hello-sections '(notmuch-hello-insert-saved-searches))
-  (setq notmuch-show-all-tags-list nil)
+  (setq notmuch-hello-sections '(notmuch-hello-insert-saved-searches notmuch-hello-insert-alltags))
+  (setq notmuch-show-all-tags-list t)
 
 ;;; Search
   (setq notmuch-search-oldest-first nil)
@@ -2452,7 +2434,7 @@ sure this is a good approach."
 ;;; Hooks and key bindings
   (add-hook 'notmuch-mua-send-hook #'notmuch-mua-attachment-check)
   (remove-hook 'notmuch-show-hook #'notmuch-show-turn-on-visual-line-mode)
-  (remove-hook 'notmuch-search-hook 'notmuch-hl-line-mode) ; CHECK
+  (remove-hook 'notmuch-search-hook 'notmuch-hl-line-mode) ; Check my `lin' package
   (add-hook 'notmuch-show-hook (lambda () (setq-local header-line-format nil)))
 
   (let ((map global-map))
@@ -2482,11 +2464,6 @@ sure this is a good approach."
           (,(kbd "r") ("-unread") "Mark as read")
           (,(kbd "u") ("+unread") "Mark as unread")))
 
-  (setq prot-notmuch-search-field-width 100)
-  (setq notmuch-hello-sections '(prot-notmuch-hello-insert-saved-searches
-                                 ;; prot-notmuch-hello-insert-recent-searches
-                                 ))
-
   (add-to-list 'notmuch-tag-formats
                '("encrypted" (propertize tag 'face 'prot-notmuch-encrypted-tag)))
   (add-to-list 'notmuch-tag-formats
@@ -2509,8 +2486,6 @@ sure this is a good approach."
   ;; Mode line indicator with the number of new mails.
   (prot-notmuch-mail-indicator 1)
 
-  (add-hook 'notmuch-hello-mode-hook #'prot-notmuch-widget-field-face-remap)
-
   (let ((map notmuch-search-mode-map))
     (define-key map (kbd "a") nil) ; the default is too easy to hit accidentally
     (define-key map (kbd "A") #'notmuch-search-archive-thread)
@@ -2529,162 +2504,14 @@ sure this is a good approach."
     (define-key map (kbd "C") #'prot-notmuch-show-complete-message)
     (define-key map (kbd "S") #'prot-notmuch-show-spam-message)))
 
-;;; ol-notmuch.el --- Links to notmuch messages
+(prot-emacs-elpa-package 'ol-notmuch)
 
-;; Copyright (C) 2010-2014  Matthieu Lemerre
-
-;; Author: Matthieu Lemerre <racin@free.fr>
-;; Keywords: outlines, hypermedia, calendar, wp
-;; Homepage: https://orgmode.org
-
-;; This file is not part of GNU Emacs.
-
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-
-;; This file is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
-
-;;; Commentary:
-
-;; This file implements links to notmuch messages and "searches". A
-;; search is a query to be performed by notmuch; it is the equivalent
-;; to folders in other mail clients. Similarly, mails are referred to
-;; by a query, so both a link can refer to several mails.
-
-;; Links have one the following form
-;; notmuch:<search terms>
-;; notmuch-search:<search terms>.
-
-;; The first form open the queries in notmuch-show mode, whereas the
-;; second link open it in notmuch-search mode. Note that queries are
-;; performed at the time the link is opened, and the result may be
-;; different from when the link was stored.
-
-;;; Code:
-
-(require 'ol)
-
-;; customisable notmuch open functions
-(defcustom org-notmuch-open-function
-  'org-notmuch-follow-link
-  "Function used to follow notmuch links.
-
-Should accept a notmuch search string as the sole argument."
-  :group 'org-notmuch
-  :version "24.4"
-  :package-version '(Org . "8.0")
-  :type 'function)
-
-(defcustom org-notmuch-search-open-function
-  'org-notmuch-search-follow-link
-  "Function used to follow notmuch-search links.
-Should accept a notmuch search string as the sole argument."
-  :group 'org-notmuch
-  :version "24.4"
-  :package-version '(Org . "8.0")
-  :type 'function)
-
-(make-obsolete-variable 'org-notmuch-search-open-function nil "9.3")
-
-
-
-;; Install the link type
-(org-link-set-parameters "notmuch"
-			 :follow #'org-notmuch-open
-			 :store #'org-notmuch-store-link)
-
-(defun org-notmuch-store-link ()
-  "Store a link to a notmuch search or message."
-  (when (memq major-mode '(notmuch-show-mode notmuch-tree-mode))
-    (let* ((message-id (notmuch-show-get-message-id t))
-	   (subject (notmuch-show-get-subject))
-	   (to (notmuch-show-get-to))
-	   (from (notmuch-show-get-from))
-	   (date (org-trim (notmuch-show-get-date)))
-	   desc link)
-      (org-link-store-props :type "notmuch" :from from :to to :date date
-       			    :subject subject :message-id message-id)
-      (setq desc (org-link-email-description))
-      (setq link (concat "notmuch:id:" message-id))
-      (org-link-add-props :link link :description desc)
-      link)))
-
-(defun org-notmuch-open (path _)
-  "Follow a notmuch message link specified by PATH."
-  (funcall org-notmuch-open-function path))
-
-(defun org-notmuch-follow-link (search)
-  "Follow a notmuch link to SEARCH.
-
-Can link to more than one message, if so all matching messages are shown."
-  (require 'notmuch)
-  (notmuch-show search))
-
-
-
-(org-link-set-parameters "notmuch-search"
-			 :follow #'org-notmuch-search-open
-			 :store #'org-notmuch-search-store-link)
-
-(defun org-notmuch-search-store-link ()
-  "Store a link to a notmuch search or message."
-  (when (eq major-mode 'notmuch-search-mode)
-    (let ((link (concat "notmuch-search:" notmuch-search-query-string))
-	  (desc (concat "Notmuch search: " notmuch-search-query-string)))
-      (org-link-store-props :type "notmuch-search"
-			    :link link
-			    :description desc)
-      link)))
-
-(defun org-notmuch-search-open (path _)
-  "Follow a notmuch message link specified by PATH."
-  (message "%s" path)
-  (org-notmuch-search-follow-link path))
-
-(defun org-notmuch-search-follow-link (search)
-  "Follow a notmuch link by displaying SEARCH in notmuch-search mode."
-  (require 'notmuch)
-  (notmuch-search search))
-
-
-
-(org-link-set-parameters "notmuch-tree"
-			 :follow #'org-notmuch-tree-open
-			 :store #'org-notmuch-tree-store-link)
-
-(defun org-notmuch-tree-store-link ()
-  "Store a link to a notmuch search or message."
-  (when (eq major-mode 'notmuch-tree-mode)
-    (let ((link (concat "notmuch-tree:" (notmuch-tree-get-query)))
-	  (desc (concat "Notmuch tree: " (notmuch-tree-get-query))))
-      (org-link-store-props :type "notmuch-tree"
-			    :link link
-			    :description desc)
-      link)))
-
-(defun org-notmuch-tree-open (path _)
-  "Follow a notmuch message link specified by PATH."
-  (message "%s" path)
-  (org-notmuch-tree-follow-link path))
-
-(defun org-notmuch-tree-follow-link (search)
-  "Follow a notmuch link by displaying SEARCH in notmuch-tree mode."
-  (require 'notmuch)
-  (notmuch-tree search))
-
-(provide 'ol-notmuch)
-
-;;; ol-notmuch.el ends here
-
-(require 'ol-notmuch)
+(prot-emacs-elpa-package 'notmuch-transient
+  (setq notmuch-transient-prefix (kbd "?"))
+  (define-key notmuch-hello-mode-map  notmuch-transient-prefix #'notmuch-hello-mode-transient)
+  (define-key notmuch-tree-mode-map   notmuch-transient-prefix #'notmuch-tree-mode-transient)
+  (define-key notmuch-search-mode-map notmuch-transient-prefix #'notmuch-search-mode-transient)
+  (define-key notmuch-show-mode-map   notmuch-transient-prefix #'notmuch-show-mode-transient))
 
 ;;; Sending email (SMTP)
 (prot-emacs-builtin-package 'smtpmail
