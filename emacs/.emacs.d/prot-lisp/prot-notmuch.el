b07da11cc5175278257616a21bf2f59f1554d6c9
Protesilaos Stavrou <info@protesilaos.com>
2022-04-19 10:42:22 +0300

    prot-notmuch: ask for SourceHut control code
    Also make the implementation a bit more abstract.
    
    I have not tested this in a real-world scenario, though the functions do
    return the expected values.

--- emacs/.emacs.d/prot-lisp/prot-notmuch.el
+++ emacs/.emacs.d/prot-lisp/prot-notmuch.el
@@ -361,6 +361,32 @@ update the mode line indicator with the new mail count."
 See `prot-notmuch-patch-add-email-control-code' for how to apply
 them.")
 
+(defun prot-notmuch--rx-in-sourcehut-mail (rx-group string)
+  "Return RX-GROUP of SourceHut mail in STRING."
+  (string-clean-whitespace
+   (replace-regexp-in-string
+    ".*?\\(\\([-a-zA-Z0-9=._+~<>/]+\\)@\\(lists.sr.ht\\)\\).*?"
+    (format "\\%s" rx-group) string)))
+
+(declare-function notmuch-show-get-header "notmuch-show" (header &optional props))
+
+(defun prot-notmuch--get-to-or-cc-header ()
+  "Get appropriate To or Cc header."
+  (cond
+   ((derived-mode-p 'notmuch-message-mode)
+    (concat (message-fetch-field "To") " " (message-fetch-field "Cc")))
+   ((derived-mode-p 'notmuch-show-mode)
+    (concat (notmuch-show-get-header :To) " " (notmuch-show-get-header :Cc)))))
+
+;; NOTE 2022-04-19: This assumes that we only have one list...  I think
+;; that is okay, but it might cause problems.
+(defun prot-notmuch--extract-sourcehut-mail (rx-group)
+  "Extract RX-GROUP from SourceHut mailing list address.
+1 is the full email address, 2 is the local part, while 3 is the
+domain."
+  (prot-notmuch--rx-in-sourcehut-mail
+   rx-group (prot-notmuch--get-to-or-cc-header)))
+
 (declare-function message-fetch-field "message" (header &optional first))
 (declare-function message-add-header "message" (&rest headers))
 
@@ -368,9 +394,7 @@ them.")
 ;;;###autoload
 (defun prot-notmuch-patch-add-email-control-code (control-code)
   "Add custom header for SourceHut email controls.
-The CONTROL-CODE is one of these special keywords (capitalisation
-is canonical): PROPOSED, NEEDS_REVISION, SUPERSEDED, APPROVED,
-REJECTED, APPLIED."
+The CONTROL-CODE is among `prot-notmuch-patch-control-codes'."
   (interactive
    (list (completing-read "Select control code: " prot-notmuch-patch-control-codes nil t)))
   (if (member control-code prot-notmuch-patch-control-codes)
@@ -378,12 +402,23 @@ REJECTED, APPLIED."
       (message-add-header (format "X-Sourcehut-Patchset-Update: %s" control-code)))
     (user-error "%s is not specified in `prot-notmuch-patch-control-codes'" control-code)))
 
+;;;###autoload
+(defun prot-notmuch-ask-sourcehut-control-code ()
+  "Use `prot-notmuch-patch-add-email-control-code' programmatically.
+Add this to `notmuch-mua-send-hook'."
+  (when-let* ((header (message-fetch-field "Subject"))
+              (subject (when (>= (length header) 6) (substring header 0 6)))
+              ((string= "[PATCH" subject)) ; Is [ always there?
+              ((prot-notmuch--extract-sourcehut-mail 1))
+              ((not (message-fetch-field "X-Sourcehut-Patchset-Update")))
+              ((y-or-n-p "Add control code for SourceHut PATCH?")))
+    (call-interactively #'prot-notmuch-patch-add-email-control-code)))
+
 ;; NOTE 2022-04-19: Ideally we should be able to use the
 ;; `notmuch-show-stash-mlarchive-link-alist' for
 ;; `prot-notmuch-stash-sourcehut-link', but it assumes that the base URL
 ;; is fixed for all message IDs, whereas those on SourceHut are not.
 
-(declare-function notmuch-show-get-header "notmuch-show" (header &optional props))
 (declare-function notmuch-show-get-message-id "notmuch-show" (&optional bare))
 (declare-function notmuch-show-message-top "notmuch-show")
 (declare-function notmuch-common-do-stash "notmuch-lib" (text))
@@ -397,9 +432,7 @@ message, else use the topmost message (start of the thread).
 Note that the topmost message is assumed to hold the id of the
 base URL, though this is not necessarily true."
   (interactive "P")
-  (let* ((to (concat (notmuch-show-get-header :To) " "
-                     (notmuch-show-get-header :Cc)))
-         (ml (replace-regexp-in-string ".*?\\([-a-zA-Z0-9=._+~<>/]+\\)@\\(lists.sr.ht\\).*?" "\\1" to))
+  (let* ((ml (prot-notmuch--extract-sourcehut-mail 2))
          (base-id (save-excursion (goto-char (point-min))
                                   (notmuch-show-message-top)
                                   (notmuch-show-get-message-id t)))
