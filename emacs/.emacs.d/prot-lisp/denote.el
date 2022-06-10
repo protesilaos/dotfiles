ffaa8d4c5b6a05ad28c8bef1d1ccbb4af919f876
Protesilaos Stavrou <info@protesilaos.com>
2022-06-10 14:24:34 +0300

    denote: sync latest updates
    Refer to its repo for documentation and proper commit/change logs:
    <https://git.sr.ht/~protesilaos/denote>.

--- emacs/.emacs.d/prot-lisp/denote.el
+++ emacs/.emacs.d/prot-lisp/denote.el
@@ -24,13 +24,38 @@
 
 ;;; Commentary:
 ;;
-;; "Denote" is the familiar word, though it also is a play on to the
-;; "note" concept.  Plus, we can come up with acronyms like:
+;; Denote is a simple, yet effective note-taking tool for Emacs.  It is
+;; based on the principle that notes must follow a predictable and
+;; descriptive file-naming scheme.  The file name alone should offer a
+;; clear indication of what the note is about, without reference to any
+;; other metadata.  Denote basically streamlines the creation of such
+;; files.
 ;;
-;; * Don't Ever Note Only The Ephemeral
-;; * Denote Everything Neatly; Omit The Excesses
+;; What Denote prioritizes with the enforcement of a strict file-naming
+;; scheme is portability.  Notes can be accessed, filtered, and understood
+;; without Emacs or any other advanced tool for that matter (though Emacs,
+;; Org, and others are excellent programs).
 ;;
-;; But I'll let you get back to work.  Don't Escape or Neglect your
+;; Denote has no mechanism to test for adherence to a given note-taking
+;; method, such as that of Zettelkasten (i.e. the contemporary digital
+;; equivalent of Niklas Luhmann's methodology).  It is possible to employ
+;; such a method, though it is ultimately up to the user to apply the
+;; requisite rigor.  What matters for our purposes is that Denote is not a
+;; zettelkasten implementation per se.
+;;
+;; By default, Denote creates note files using the `.org' extension.
+;; However, Denote does not depend on org.el or any of its accoutrements
+;; and extensions.  Users are given the option to change from Org to either
+;; Markdown (`.md') or Plain Text (`.txt'), as explained further in the
+;; manual (search for `denote-file-type').
+;;
+;; "Denote" is the familiar word, though it also is a play on to the "note"
+;; concept.  Plus, we can come up with acronyms (of dubious utility) like:
+;;
+;; + Don't Ever Note Only The Ephemeral
+;; + Denote Everything Neatly; Omit The Excesses
+;;
+;; But we'll let you get back to work.  Don't Escape or Neglect your
 ;; Obligations, Tasks, Engagements...
 
 ;;; Code:
@@ -85,6 +110,23 @@ If nil, show the keywords in their given order."
   :group 'denote
   :type 'boolean)
 
+(defcustom denote-file-type nil
+  "The file type extension for new notes.
+
+By default (a nil value), the file type is that of Org mode.
+
+When the value is the symbol `markdown', the file type is that of
+Markdown mode.
+
+When the value is `text', the file type is that of Text mode.
+
+Any other non-nil value is the same as the default."
+  :type '(choice
+          (const :tag "Org mode (default)" nil)
+          (const :tag "Markdown" markdown)
+          (const :tag "Plain text" text))
+  :group 'denote)
+
 (defcustom denote-front-matter-date-format nil
   "Date format in the front matter (file header) of new notes.
 
@@ -105,18 +147,19 @@ specifiers."
 
 ;;;; Main variables
 
-(defconst denote--id "%Y%m%d_%H%M%S"
+(defconst denote--id "%Y%m%dT%H%M%S"
   "Format of ID prefix of a note's filename.")
 
-(defconst denote--id-regexp "\\([0-9_]+\\{15\\}\\)"
+(defconst denote--id-regexp "\\([0-9]\\{8\\}\\)\\(T[0-9]\\{6\\}\\)"
   "Regular expression to match `denote--id'.")
 
-(defconst denote--keyword-regexp "\\(--\\)\\([0-9A-Za-z_+]*\\)\\(--\\)"
-  "Regular expression to match `denote-keywords'.")
-
 (defconst denote--file-regexp
-  (concat denote--id-regexp denote--keyword-regexp "\\(.*\\)\\.?.*")
+  (concat denote--id-regexp "\\(--\\)\\(.*\\)\\(--\\)")
   "Regular expression to match file names from `denote-new-note'.")
+
+(defconst denote--keyword-regexp
+  (concat denote--file-regexp "\\([0-9A-Za-z_+]*\\)\\(\\.?.*\\)")
+  "Regular expression to match `denote-keywords'.")
 
 (defconst denote--punctuation-regexp "[][{}!@#$%^&*()_=+'\"?,.\|;:~`‘’“”]*"
   "Regular expression of punctionation that should be removed.")
@@ -206,8 +249,7 @@ With optional N, search in the Nth line from point."
   "Produce list of keywords in `denote--directory-files'."
   (delq nil (mapcar
              (lambda (x)
-               (denote--extract
-                (concat denote--id-regexp denote--keyword-regexp) x 3))
+               (denote--extract denote--keyword-regexp x 6))
              (denote--directory-files))))
 
 (defun denote--inferred-keywords ()
@@ -253,13 +295,6 @@ output is sorted with `string-lessp'."
       (mapconcat #'downcase keywords "+")
     keywords))
 
-(defun denote--keywords-capitalize (keywords)
-  "`capitalize' KEYWORDS output of `denote--keywords-prompt'."
-  (if (and (> (length keywords) 1)
-           (not (stringp keywords)))
-      (mapconcat #'capitalize keywords ", ")
-    (capitalize keywords)))
-
 (defun denote--keywords-add-to-history (keywords)
   "Append KEYWORDS to `denote--keyword-history'."
   (if-let ((listed (listp keywords))
@@ -275,33 +310,63 @@ output is sorted with `string-lessp'."
 
 ;;;; New note
 
-(defun denote--format-file (path id keywords slug &optional extension)
+(defun denote--file-extension ()
+  "Return file type extension based on `denote-file-type'."
+  (pcase denote-file-type
+    ('markdown ".md")
+    ('text ".txt")
+    (_ ".org")))
+
+(defun denote--format-file (path id keywords slug extension)
   "Format file name.
 PATH, ID, KEYWORDS, SLUG are expected to be supplied by `denote'
 or equivalent: they will all be converted into a single string.
-
-Optional EXTENSION is the file type extension.  Use .org if none
-is specified."
+EXTENSION is the file type extension, either a string which
+include the starting dot or the return value of
+`denote--file-extension'."
   (let ((kws (if denote-infer-keywords
                  (denote--keywords-combine keywords)
                keywords))
-        (ext (or extension ".org")))
-    (format "%s%s--%s--%s%s" path id kws slug ext)))
+        (ext (or extension (denote--file-extension))))
+    (format "%s%s--%s--%s%s" path id slug kws ext)))
 
-(defun denote--file-meta-header (title date keywords filename id)
+(defun denote--file-meta-keywords (keywords)
+  "Prepare KEYWORDS for inclusion in the file's front matter.
+Parse the output of `denote--keywords-prompt', using `downcase'
+on the keywords and separating them by two spaces.  A single
+keyword is just downcased."
+  (if (and (> (length keywords) 1)
+           (not (stringp keywords)))
+      (mapconcat #'downcase keywords "  ")
+    (downcase keywords)))
+
+(defun denote--file-meta-header (title date keywords id)
   "Front matter for new notes.
 
 TITLE, DATE, KEYWORDS, FILENAME, ID are all strings which are
  provided by `denote-new-note'."
-  (let ((kw (denote--keywords-capitalize keywords)))
-    (concat "#+title:      " title     "\n"
-            "#+date:       " date      "\n"
-            "#+filetags:   " kw        "\n"
-            "#+identifier: " id        "\n"
-            "#+filename:   " (string-remove-prefix denote-directory filename)  "\n"
-            "#+path:       " filename  "\n"
-            "#+link:       " "denote " (denote-directory) "%s"
-            "\n\n")))
+  (let ((kw (denote--file-meta-keywords keywords)))
+    (pcase denote-file-type
+      ('markdown (concat "---" "\n"
+                         "title:      " title "\n"
+                         "date:       " date  "\n"
+                         "tags:       " kw    "\n"
+                         "identifier: " id    "\n"
+                         "---"                "\n"
+                         "\n"))
+
+      ('text (concat "title:      " title "\n"
+                     "date:       " date  "\n"
+                     "tags:       " kw    "\n"
+                     "identifier: " id    "\n"
+                     (make-string 27 ?-)  "\n"
+                     "\n"))
+
+      (_ (concat "#+title:      " title "\n"
+                 "#+date:       " date  "\n"
+                 "#+filetags:   " kw    "\n"
+                 "#+identifier: " id    "\n"
+                 "\n")))))
 
 (defun denote--path (title keywords)
   "Return path to new file with TITLE and KEYWORDS.
@@ -311,7 +376,8 @@ Format current time, else use optional ID."
          (file-name-as-directory denote-directory)
          (format-time-string denote--id)
          keywords
-         (denote--sluggify title))))
+         (denote--sluggify title)
+         (denote--file-extension))))
 
 (defun denote--date ()
   "Expand the date for a new note's front matter."
@@ -330,7 +396,7 @@ Use optional PATH, else create it with `denote--path'."
          (default-directory denote-directory)
          (buffer (unless path (find-file p)))
          (header (denote--file-meta-header
-                  title (denote--date) keywords p
+                  title (denote--date) keywords
                   (format-time-string denote--id))))
     (unless path
       (with-current-buffer buffer (insert header))
