734b1422479d295f098a277ff0d8caf3e2a9206a
Protesilaos Stavrou <info@protesilaos.com>
2022-06-15 19:28:44 +0300

    prot-emacs: sync .el version of configuration

--- emacs/.emacs.d/prot-emacs.el
+++ emacs/.emacs.d/prot-emacs.el
@@ -1323,11 +1323,19 @@ Useful for prompts such as `eval-expression' and `shell-command'."
   (define-key global-map (kbd "C-x r l") #'blist-list-bookmarks))
 
 (prot-emacs-builtin-package 'denote
+  ;; Remember to check the doc strings of those variables.
   (setq denote-directory (expand-file-name "~/Documents/notes/"))
   (setq denote-known-keywords
         '("emacs" "philosophy" "politics" "economics"))
   (setq denote-infer-keywords t)
   (setq denote-sort-keywords t)
+  (setq denote-file-type nil) ; Org is the default, set others here
+
+  ;; We allow multi-word keywords by default.  The author's personal
+  ;; preference is for single-word keywords for a more rigid workflow.
+  (setq denote-allow-multi-word-keywords t)
+
+  (setq denote-front-matter-date-format nil) ; change this to `org-timestamp' or custom string
 
   (require 'denote-link)
   (require 'denote-dired)
@@ -1344,13 +1352,40 @@ Useful for prompts such as `eval-expression' and `shell-command'."
   ;; OR better:
   (add-hook 'dired-mode-hook #'denote-dired-mode-in-directories)
 
-  ;; You can bind `denote' to a global key if you prefer not to use
-  ;; `org-capture' or want an alternative.  Denote does not define any key
-  ;; bindings though: this is for the user to decide.  For example:
-  (let ((map global-map))
-    (define-key map (kbd "C-c n") #'denote)
-    (define-key map (kbd "C-c N") #'denote-type))
+  ;; Here is a custom, user-level command from one of the examples we
+  ;; showed in this manual.  We define it here and add it to a key binding
+  ;; below.
+  ;;
+  ;; The manual: <https://protesilaos.com/emacs/denote>.
+  (defun prot/denote-journal ()
+    "Create an entry tagged 'journal', while prompting for a title."
+    (interactive)
+    (denote
+     (denote--title-prompt)
+     "journal"))
 
+  ;; Denote does not define any key bindings.  This is for the user to
+  ;; decide.  For example:
+  (let ((map global-map))
+    (define-key map (kbd "C-c n j") #'prot/denote-journal) ; our custom command
+    (define-key map (kbd "C-c n n") #'denote)
+    (define-key map (kbd "C-c n N") #'denote-type)
+    ;; If you intend to use Denote with a variety of file types, it is
+    ;; easier to bind the link-related commands to the `global-map', as
+    ;; shown here.  Otherwise follow the same pattern for `org-mode-map',
+    ;; `markdown-mode-map', and/or `text-mode-map'.
+    (define-key map (kbd "C-c n i") #'denote-link) ; "insert" mnemonic
+    (define-key map (kbd "C-c n I") #'denote-link-add-links)
+    (define-key map (kbd "C-c n l") #'denote-link-find-file) ; "list" links
+    (define-key map (kbd "C-c n b") #'denote-link-backlinks)
+    ;; Note that `denote-dired-rename-file' can work from any context, not
+    ;; just Dired bufffers.  That is why we bind it here to the
+    ;; `global-map'.
+    (define-key map (kbd "C-c n r") #'denote-dired-rename-file))
+
+  ;; I bind `org-capture' to C-c c, so C-c c n for the note.  I use this
+  ;; one when I need to leverage the extras that Org provides, such as a
+  ;; link to an email, elfeed entry, and more.
   (with-eval-after-load 'org-capture
     (require 'denote-org-capture)
     (setq denote-org-capture-specifiers "%l\n%i\n%?")
