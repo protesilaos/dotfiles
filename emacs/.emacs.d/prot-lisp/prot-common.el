5d8b8236551b2657b58c84eaa8f1bbc7acd44e70
Protesilaos Stavrou <info@protesilaos.com>
2023-03-22 15:22:14 +0200

    prot-common: revise prot-common-url-regexp

--- emacs/.emacs.d/prot-lisp/prot-common.el
+++ emacs/.emacs.d/prot-lisp/prot-common.el
@@ -252,23 +252,10 @@ Return the exit code and output in a list."
 
 (defvar prot-common-url-regexp
   (concat
-   "\\b\\(\\(www\\.\\|\\(s?https?\\|ftp\\|file\\|gopher\\|"
-   "nntp\\|news\\|telnet\\|wais\\|mailto\\|info\\):\\)"
-   "\\(//[-a-z0-9_.]+:[0-9]*\\)?"
-   (let ((chars "-a-z0-9_=#$@~%&*+\\/[:word:]")
-	     (punct "!?:;.,"))
-     (concat
-      "\\(?:"
-      ;; Match paired parentheses, e.g. in Wikipedia URLs:
-      ;; http://thread.gmane.org/47B4E3B2.3050402@gmail.com
-      "[" chars punct "]+" "(" "[" chars punct "]+" ")"
-      "\\(?:" "[" chars punct "]+" "[" chars "]" "\\)?"
-      "\\|"
-      "[" chars punct "]+" "[" chars "]"
-      "\\)"))
-   "\\)")
-  "Regular expression that matches URLs.
-Copy of variable `browse-url-button-regexp'.")
+   "~?\\<\\([-a-zA-Z0-9+&@#/%?=~_|!:,.;]*\\)"
+   "[.@]"
+   "\\([-a-zA-Z0-9+&@#/%?=~_|!:,.;]+\\)\\>")
+  "Regular expression to match (most?) URLs or email addresses.")
 
 (autoload 'auth-source-search "auth-source")
 
