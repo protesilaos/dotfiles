afde06fe58c3b8f99c4b751cad6aa4318dc80dff
Protesilaos Stavrou <info@protesilaos.com>
2023-05-05 08:37:39 +0300

    prot-emacs-langs: tweak jinx configuration (EXPERIMENTAL)

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-langs.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-langs.el
@@ -96,10 +96,21 @@
   (:install t)
   (:delay 10)
   (setq jinx-languages "en_GB el_GR fr_FR es_ES")
-
+  (setq jinx-include-modes '(text-mode prog-mode))
+  (setq jinx-include-faces
+        '((prog-mode font-lock-doc-face)
+          (conf-mode font-lock-comment-face)))
+  (setq jinx-exclude-regexps
+        '((t "[A-Z]+\\>"
+             "\\<[[:upper:]][[:lower:]]+\\>"
+             "\\w*?[0-9\.'\"-]\\w*"
+             "[a-z]+://\\S-+"
+             "<?[-+_.~a-zA-Z][-+_.~:a-zA-Z0-9]*@[-.a-zA-Z0-9]+>?")))
+  
   (global-jinx-mode 1)
 
   (prot-emacs-keybind global-map
+    "<f8>" #'jinx-mode
     "M-$" #'jinx-correct
     "C-M-$" #'jinx-languages))
 
