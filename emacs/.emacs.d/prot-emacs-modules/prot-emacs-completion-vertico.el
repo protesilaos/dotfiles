b651786b4f58fc5ce033fa43d81a9fffef13cb19
Protesilaos Stavrou <info@protesilaos.com>
2023-07-23 13:40:29 +0300

    prot-vertico: put custom code in its own file

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-completion-vertico.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-completion-vertico.el
@@ -2,10 +2,6 @@
 (prot-emacs-package vertico
   (:install t)
   (:delay 5)
-  ;; Those are the default values, but check the user option
-  ;; `vertico-multiform-categories' for per-category tweaks.  That
-  ;; variable is in the file vertico-multiform.el and will work once
-  ;; `vertico-multiform-mode' is enabled.
   (setq vertico-scroll-margin 0)
   (setq vertico-count 5)
   (setq vertico-resize nil)
@@ -13,83 +9,29 @@
 
   (vertico-mode 1)
 
-  (defvar prot-vertico-minimal
-    '(unobtrusive
-      (vertico-flat-format . ( :multiple  ""
-                               :single    ""
-                               :prompt    ""
-                               :separator ""
-                               :ellipsis  ""
-                               :no-match  "")))
-    "List of configurations for minimal Vertico multiform.
-The minimal view is intended to be more private or less
-revealing.  This is important when, for example, a prompt shows
-names of people.  Of course, such a view also provides a minimal
-style for general usage.
+  ;; This works with `file-name-shadow-mode' enabled.  When you are in
+  ;; a sub-directory and use, say, `find-file' to go to your home '~/'
+  ;; or root '/' directory, Vertico will clear the old path to keep
+  ;; only your current input.
+  (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy))
 
-Toggle the vertical view with the `vertico-multiform-vertical'
-command or use the commands `prot-vertico-private-next' and
-`prot-vertico-private-previous', which toggle the vertical view
-automatically.")
-
-  (defvar prot-vertico-maximal
-    '((vertico-count . 10)
-      (vertico-resize . t))
-    "List of configurations for maximal Vertico multiform.")
-
-  ;; Sort directories before files.  From the Vertico documentation.
-  (defun contrib/sort-directories-first (files)
-    (setq files (vertico-sort-history-length-alpha files))
-    (nconc (seq-filter (lambda (x) (string-suffix-p "/" x)) files)
-           (seq-remove (lambda (x) (string-suffix-p "/" x)) files)))
-
+;;; Custom tweaks for vertico (prot-vertico.el)
+(prot-emacs-package prot-vertico
+  (:delay 5)
   (setq vertico-multiform-categories
         `(;; Maximal
-          (embark-keybinding ,@prot-vertico-maximal)
-          (multi-category ,@prot-vertico-maximal)
-          (consult-location ,@prot-vertico-maximal)
-          (imenu ,@prot-vertico-maximal)
-          (unicode-name ,@prot-vertico-maximal)
+          (embark-keybinding ,@prot-vertico-multiform-maximal)
+          (multi-category ,@prot-vertico-multiform-maximal)
+          (consult-location ,@prot-vertico-multiform-maximal)
+          (imenu ,@prot-vertico-multiform-maximal)
+          (unicode-name ,@prot-vertico-multiform-maximal)
           ;; Minimal
-          (file ,@prot-vertico-minimal
+          (file ,@prot-vertico-multiform-minimal
                 (vertico-preselect . prompt)
-                (vertico-sort-function . contrib/sort-directories-first))
-          (t ,@prot-vertico-minimal)))
+                (vertico-sort-function . prot-vertico-sort-directories-first))
+          (t ,@prot-vertico-multiform-minimal)))
 
   (vertico-multiform-mode 1)
-
-  (defun prot-vertico-private-next ()
-    "Like `vertico-next' but toggle vertical view if needed.
-This is done to accommodate `prot-vertico-minimal'."
-    (interactive)
-    (if vertico-unobtrusive-mode
-        (let ((vertico--index 0))
-          (vertico-multiform-vertical)
-          (vertico-next 1))
-      (vertico-next 1)))
-
-  (defun prot-vertico-private-previous ()
-    "Like `vertico-previous' but toggle vertical view if needed.
-This is done to accommodate `prot-vertico-minimal'."
-    (interactive)
-    (if vertico-unobtrusive-mode
-        (progn
-          (vertico-multiform-vertical)
-          (vertico-previous 1))
-      (vertico-previous 1)))
-
-  (defun prot-vertico-private-complete ()
-    "Expand contents and show remaining candidates, if needed.
-This is done to accommodate `prot-vertico-minimal'."
-    (interactive)
-    (cond
-     ((and vertico-unobtrusive-mode (> vertico--total 1))
-      (minibuffer-complete)
-      (vertico-multiform-vertical))
-     ((and vertico-unobtrusive-mode (= vertico--total 1))
-      (minibuffer-force-complete-and-exit))
-     (t
-      (vertico-insert))))
 
   (prot-emacs-keybind vertico-map
     "<left>" #'backward-char
@@ -105,12 +47,6 @@ This is done to accommodate `prot-vertico-minimal'."
     "<down>" #'prot-vertico-private-next
     "C-p" #'prot-vertico-private-previous
     "<up>" #'prot-vertico-private-previous
-    "C-l" #'vertico-multiform-vertical)
-
-  ;; This works with `file-name-shadow-mode' enabled.  When you are in
-  ;; a sub-directory and use, say, `find-file' to go to your home '~/'
-  ;; or root '/' directory, Vertico will clear the old path to keep
-  ;; only your current input.
-  (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy))
+    "C-l" #'vertico-multiform-vertical))
 
 (provide 'prot-emacs-completion-vertico)
---
+++ emacs/.emacs.d/prot-lisp/prot-vertico.el
@@ -0,0 +1,105 @@
+;;; prot-vertico.el --- Code for my custom mode line -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023  Protesilaos Stavrou
+
+;; Author: Protesilaos Stavrou <info@protesilaos.com>
+;; URL: https://protesilaos.com/emacs/dotemacs
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "30.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Remember that every piece of Elisp that I write is for my own
+;; educational and recreational purposes.  I am not a programmer and I
+;; do not recommend that you copy any of this if you are not certain of
+;; what it does.
+
+;;; Code:
+
+(require 'vertico)
+
+(defvar prot-vertico-multiform-minimal
+  '(unobtrusive
+    (vertico-flat-format . ( :multiple  ""
+                             :single    ""
+                             :prompt    ""
+                             :separator ""
+                             :ellipsis  ""
+                             :no-match  "")))
+  "List of configurations for minimal Vertico multiform.
+The minimal view is intended to be more private or less
+revealing.  This is important when, for example, a prompt shows
+names of people.  Of course, such a view also provides a minimal
+style for general usage.
+
+Toggle the vertical view with the `vertico-multiform-vertical'
+command or use the commands `prot-vertico-private-next' and
+`prot-vertico-private-previous', which toggle the vertical view
+automatically.")
+
+(defvar prot-vertico-multiform-maximal
+  '((vertico-count . 10)
+    (vertico-resize . t))
+  "List of configurations for maximal Vertico multiform.")
+
+(defun prot-vertico--match-directory (str)
+  "Match directory delimiter in STR."
+  (string-suffix-p "/" str))
+
+;; From the Vertico documentation.
+(defun prot-vertico-sort-directories-first (files)
+  "Sort directories before FILES."
+  (setq files (vertico-sort-alpha files))
+  (nconc (seq-filter #'prot-vertico--match-directory files)
+         (seq-remove #'prot-vertico--match-directory files)))
+
+(defun prot-vertico-private-next ()
+  "Like `vertico-next' but toggle vertical view if needed.
+This is done to accommodate `prot-vertico-multiform-minimal'."
+  (interactive)
+  (if vertico-unobtrusive-mode
+      (let ((vertico--index 0))
+        (vertico-multiform-vertical)
+        (vertico-next 1))
+    (vertico-next 1)))
+
+(defun prot-vertico-private-previous ()
+  "Like `vertico-previous' but toggle vertical view if needed.
+This is done to accommodate `prot-vertico-multiform-minimal'."
+  (interactive)
+  (if vertico-unobtrusive-mode
+      (progn
+        (vertico-multiform-vertical)
+        (vertico-previous 1))
+    (vertico-previous 1)))
+
+(defun prot-vertico-private-complete ()
+  "Expand contents and show remaining candidates, if needed.
+This is done to accommodate `prot-vertico-multiform-minimal'."
+  (interactive)
+  (cond
+   ((and vertico-unobtrusive-mode (> vertico--total 1))
+    (minibuffer-complete)
+    (vertico-multiform-vertical))
+   ((and vertico-unobtrusive-mode (= vertico--total 1))
+    (minibuffer-force-complete-and-exit))
+   (t
+    (vertico-insert))))
+
+(provide 'prot-vertico)
+;;; prot-vertico.el ends here
