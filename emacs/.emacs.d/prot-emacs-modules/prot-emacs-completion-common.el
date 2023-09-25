191f0012bbe1deb28014f6e1e334028bb63fedd3
Protesilaos Stavrou <info@protesilaos.com>
2023-09-25 07:36:23 +0300

    prot-emacs-completion-common: clarify comment

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-completion-common.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-completion-common.el
@@ -89,9 +89,11 @@
   (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
   ;; Add prompt indicator to `completing-read-multiple'.  We display
-  ;; [CRM<separator>], e.g., [CRM,] if the separator is a comma.  This
-  ;; is copied from the README of the `vertico' package.  I made some
-  ;; small tweaks to propertize the segments of the prompt.
+  ;; [`completing-read-multiple': <separator>], e.g.,
+  ;; [`completing-read-multiple': ,] if the separator is a comma.  This
+  ;; is adapted from the README of the `vertico' package by Daniel
+  ;; Mendler.  I made some small tweaks to propertize the segments of
+  ;; the prompt.
   (defun crm-indicator (args)
     (cons (format "[`crm-separator': %s]  %s"
                   (propertize
