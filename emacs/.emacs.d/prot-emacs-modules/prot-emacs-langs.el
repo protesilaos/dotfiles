e876783d0d61ca6fdf79021255210b8a2cc26c94
Protesilaos Stavrou <info@protesilaos.com>
2023-09-28 09:10:37 +0300

    prot-emacs-langs: remove unused comment keys

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-langs.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-langs.el
@@ -80,11 +80,7 @@
   (setq comment-fill-column nil)
   (setq comment-multi-line t)
   (setq comment-style 'multi-line)
-  (setq-default comment-column 0)
-
-  (prot-emacs-keybind global-map
-    "C-:" comment-kill ; C-S-;
-    "M-;" comment-indent))
+  (setq-default comment-column 0))
 
 (prot-emacs-package prot-comment
   (:delay 5)
