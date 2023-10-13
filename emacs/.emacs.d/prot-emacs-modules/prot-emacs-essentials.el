688c0febca557310eb6a21c9ae80ac36363dab21
Protesilaos Stavrou <info@protesilaos.com>
2023-10-13 15:45:52 +0300

    prot-emacs-essentials: reduce delete-pair-blink-delay

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-essentials.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-essentials.el
@@ -14,7 +14,7 @@
   (require 'prot-comment)
 
 ;;;; General settings and common custom functions (prot-simple.el)
-  (setq delete-pair-blink-delay 0.15) ; Emacs28 -- see `prot-simple-delete-pair-dwim'
+  (setq delete-pair-blink-delay 0.1) ; Emacs28 -- see `prot-simple-delete-pair-dwim'
   (setq help-window-select t)
   (setq next-error-recenter '(4)) ; center of the window
   (setq find-library-include-other-files nil) ; Emacs 29
