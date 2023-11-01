35492414b78d586edea7d0b17dd4cccf584e4a1b
Protesilaos Stavrou <info@protesilaos.com>
2023-11-01 09:46:12 +0200

    prot-emacs-which-key: add missing file
    I forgot to commit it...

---
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
@@ -0,0 +1,13 @@
+(prot-emacs-package which-key
+  (:install t)
+  (:delay 1)
+  (setq which-key-separator " ")
+  (setq which-key-max-display-columns 3)
+  (setq which-key-idle-delay 2)
+  (setq which-key-idle-secondary-delay 0.05)
+  (setq which-key-add-column-padding 5)
+  (setq which-key-max-description-length 40)
+
+  (which-key-mode 1))
+  
+(provide 'prot-emacs-which-key)
