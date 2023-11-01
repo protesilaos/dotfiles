4d20961b30172494a1ca31dfef38ee2c00941fd5
Protesilaos Stavrou <info@protesilaos.com>
2023-11-01 10:54:23 +0200

    prot-emacs-which-key: change the prefix for keymaps to stand out more

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
@@ -2,6 +2,7 @@
   (:install t)
   (:delay 1)
   (setq which-key-separator " ")
+  (setq which-key-prefix-prefix "... ")
   (setq which-key-max-display-columns 3)
   (setq which-key-idle-delay 2)
   (setq which-key-idle-secondary-delay 0.05)
