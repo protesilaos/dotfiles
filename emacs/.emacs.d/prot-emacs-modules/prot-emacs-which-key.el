f8a45afe490058ba46ada798a94dcba6efe8d98a
Protesilaos Stavrou <info@protesilaos.com>
2023-11-01 13:19:09 +0200

    prot-emacs-which-key: increase secondary delay

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
@@ -5,7 +5,7 @@
   (setq which-key-prefix-prefix "... ")
   (setq which-key-max-display-columns 3)
   (setq which-key-idle-delay 0.5)
-  (setq which-key-idle-secondary-delay 0.05)
+  (setq which-key-idle-secondary-delay 0.25)
   (setq which-key-add-column-padding 5)
   (setq which-key-max-description-length 40)
 
