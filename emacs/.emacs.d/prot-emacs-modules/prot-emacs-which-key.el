4a454f482f35aa74168d767ed9adcee3191ecca3
Protesilaos Stavrou <info@protesilaos.com>
2023-11-06 13:40:16 +0200

    prot-emacs-which-key: make minor tweaks

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
@@ -1,14 +1,14 @@
 (prot-emacs-package which-key
   (:install t)
   (:delay 1)
-  (setq which-key-separator " ")
+  (setq which-key-separator "  ")
   (setq which-key-prefix-prefix "... ")
   (setq which-key-max-display-columns 3)
   (setq which-key-idle-delay 0.5)
   (setq which-key-idle-secondary-delay 0.25)
-  (setq which-key-add-column-padding 5)
+  (setq which-key-add-column-padding 1)
   (setq which-key-max-description-length 40)
 
   (which-key-mode 1))
-  
+
 (provide 'prot-emacs-which-key)
