5fd64341583c0aab604e95c01715d07a0aa96800
Protesilaos Stavrou <info@protesilaos.com>
2023-11-15 09:43:26 +0200

    early-init: tweak startup optimisations and adjust GC
    I am still experimenting with the default GC values, though these ones
    seem okay.

--- emacs/.emacs.d/early-init.el
+++ emacs/.emacs.d/early-init.el
@@ -73,24 +73,26 @@ Simplified version of `add-to-list'."
 (tool-bar-mode -1)
 
 ;; Temporarily increase the garbage collection threshold.  These
-;; changes help shave off about half a second of startup time.
-(defvar prot-emacs--gc-cons-threshold gc-cons-threshold)
+;; changes help shave off about half a second of startup time.  The
+;; `most-positive-fixnum' is DANGEROUS AS A PERMANENT VALUE.  See the
+;; `emacs-startup-hook' a few lines below for what I actually use.
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.5)
 
-(setq gc-cons-threshold most-positive-fixnum)
-
-;; Same idea as above for the `file-name-handler-alist'.
+;; Same idea as above for the `file-name-handler-alist' and the
+;; `vc-handled-backends' with regard to startup speed optimisation.
+;; Here I am storing the default value with the intent of restoring it
+;; via the `emacs-startup-hook'.
 (defvar prot-emacs--file-name-handler-alist file-name-handler-alist)
-
-(setq file-name-handler-alist nil)
-
-;; Same idea as above for the `vc-handled-backends'.
 (defvar prot-emacs--vc-handled-backends vc-handled-backends)
 
-(setq vc-handled-backends nil)
+(setq file-name-handler-alist nil
+      vc-handled-backends nil)
 
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold prot-emacs--gc-cons-threshold
+            (setq gc-cons-threshold (* 1024 1024 10)
+                  gc-cons-percentage 0.2
                   file-name-handler-alist prot-emacs--file-name-handler-alist
                   vc-handled-backends prot-emacs--vc-handled-backends)))
 
