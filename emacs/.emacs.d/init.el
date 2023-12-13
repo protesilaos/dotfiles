3db50757693c0afde87329208b338dcea4e53a01
Protesilaos Stavrou <info@protesilaos.com>
2023-12-13 06:52:57 +0200

    init.el: tweak how native compilation variables are set

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -95,11 +95,14 @@ before all other modules of my setup."
 (setq frame-title-format '("%b"))
 (setq ring-bell-function 'ignore)
 (setq use-short-answers t)
-(setq native-comp-async-report-warnings-errors 'silent) ; Emacs 28 with native compilation
-(setq native-compile-prune-cache t) ; Emacs 29
 (setq make-backup-files nil)
 (setq backup-inhibited nil) ; Not sure if needed, given `make-backup-files'
 (setq create-lockfiles nil)
+
+;; Make native compilation silent and prune its cache.
+(when (native-comp-available-p)
+  (setq native-comp-async-report-warnings-errors 'silent) ; Emacs 28 with native compilation
+  (setq native-compile-prune-cache t)) ; Emacs 29
 
 ;; Disable the damn thing by making it disposable.
 (setq custom-file (make-temp-file "emacs-custom-"))
