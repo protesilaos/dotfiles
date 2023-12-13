eaaa971804208d0b747e860642491af4f90e6f05
Protesilaos Stavrou <info@protesilaos.com>
2023-12-13 07:02:37 +0200

    prot-emacs-modeline: add integration with my spacious-padding package

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-modeline.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-modeline.el
@@ -24,7 +24,20 @@
                   mode-line-format-right-align ; Emacs 30
                   prot-modeline-notmuch-indicator
                   "  "
-                  prot-modeline-misc-info)))
+                  prot-modeline-misc-info))
+
+  (with-eval-after-load 'spacious-padding
+    (defun prot/modeline-spacious-indicators ()
+      "Set box attribute to `'prot-modeline-indicator-button' if spacious-padding is enabled."
+      (if (bound-and-true-p spacious-padding-mode)
+          (set-face-attribute 'prot-modeline-indicator-button nil :box t)
+        (set-face-attribute 'prot-modeline-indicator-button nil :box 'unspecified)))
+
+    ;; Run it at startup and then afterwards whenever
+    ;; `spacious-padding-mode' is toggled on/off.
+    (prot/modeline-spacious-indicators)
+
+    (add-hook 'spacious-padding-mode-hook #'prot/modeline-spacious-indicators)))
 
 ;;; Keycast mode
 (prot-emacs-package keycast
--- emacs/.emacs.d/prot-lisp/prot-modeline.el
+++ emacs/.emacs.d/prot-lisp/prot-modeline.el
@@ -47,6 +47,12 @@
 
 ;;;; Faces
 
+(defface prot-modeline-indicator-button nil
+  "Generic face used for indicators that have a background.
+Modify this face to, for example, add a :box attribute to all
+relevant indicators (combines nicely with my `spacious-padding'
+package).")
+
 (defface prot-modeline-indicator-red
   '((default :inherit bold)
     (((class color) (min-colors 88) (background light))
@@ -58,7 +64,7 @@
   :group 'prot-modeline-faces)
 
 (defface prot-modeline-indicator-red-bg
-  '((default :inherit bold)
+  '((default :inherit (bold prot-modeline-indicator-button))
     (((class color) (min-colors 88) (background light))
      :background "#aa1111" :foreground "white")
     (((class color) (min-colors 88) (background dark))
@@ -78,7 +84,7 @@
   :group 'prot-modeline-faces)
 
 (defface prot-modeline-indicator-green-bg
-  '((default :inherit bold)
+  '((default :inherit (bold prot-modeline-indicator-button))
     (((class color) (min-colors 88) (background light))
      :background "#207b20" :foreground "white")
     (((class color) (min-colors 88) (background dark))
@@ -98,7 +104,7 @@
   :group 'prot-modeline-faces)
 
 (defface prot-modeline-indicator-yellow-bg
-  '((default :inherit bold)
+  '((default :inherit (bold prot-modeline-indicator-button))
     (((class color) (min-colors 88) (background light))
      :background "#805000" :foreground "white")
     (((class color) (min-colors 88) (background dark))
@@ -118,7 +124,7 @@
   :group 'prot-modeline-faces)
 
 (defface prot-modeline-indicator-blue-bg
-  '((default :inherit bold)
+  '((default :inherit (bold prot-modeline-indicator-button))
     (((class color) (min-colors 88) (background light))
      :background "#0000aa" :foreground "white")
     (((class color) (min-colors 88) (background dark))
@@ -138,7 +144,7 @@
   :group 'prot-modeline-faces)
 
 (defface prot-modeline-indicator-magenta-bg
-  '((default :inherit bold)
+  '((default :inherit (bold prot-modeline-indicator-button))
     (((class color) (min-colors 88) (background light))
      :background "#6f0f9f" :foreground "white")
     (((class color) (min-colors 88) (background dark))
@@ -158,7 +164,7 @@
   :group 'prot-modeline-faces)
 
 (defface prot-modeline-indicator-cyan-bg
-  '((default :inherit bold)
+  '((default :inherit (bold prot-modeline-indicator-button))
     (((class color) (min-colors 88) (background light))
      :background "#006080" :foreground "white")
     (((class color) (min-colors 88) (background dark))
