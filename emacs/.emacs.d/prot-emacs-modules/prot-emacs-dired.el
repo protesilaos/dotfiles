6edb24802382e054351e9e81cdaccffa6a4b777f
Protesilaos Stavrou <info@protesilaos.com>
2023-12-30 06:49:59 +0200

    emacs: tangle again the prot-emacs-dired.el

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-dired.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-dired.el
@@ -4,22 +4,26 @@
   (setq dired-recursive-copies 'always)
   (setq dired-recursive-deletes 'always)
   (setq delete-by-moving-to-trash t)
+
   (setq dired-listing-switches
         "-AGFhlv --group-directories-first --time-style=long-iso")
+
   (setq dired-dwim-target t)
+
+  (setq dired-guess-shell-alist-user ; those are the suggestions for ! and & in Dired
+        '(("\\.\\(png\\|jpe?g\\|tiff\\)" "feh" "xdg-open")
+          ("\\.\\(mp[34]\\|m4a\\|ogg\\|flac\\|webm\\|mkv\\)" "mpv" "xdg-open")
+          (".*" "xdg-open")))
+
   (setq dired-auto-revert-buffer #'dired-directory-changed-p) ; also see `dired-do-revert-buffer'
   (setq dired-make-directory-clickable t) ; Emacs 29.1
   (setq dired-free-space nil) ; Emacs 29.1
   (setq dired-mouse-drag-files t) ; Emacs 29.1
-  (setq dired-guess-shell-alist-user ; those are the suggestions for ! and & in Dired
-        '(("\\.\\(png\\|jpe?g\\|tiff\\)" "feh" "xdg-open")
-          ("\\.\\(mp[34]\\|m4a\\|ogg\\|flac\\|webm\\|mkv\\)" "mpv" "xdg-open")
-		  (".*" "xdg-open")))
 
   (add-hook 'dired-mode-hook #'dired-hide-details-mode)
   (add-hook 'dired-mode-hook #'hl-line-mode)
 
-  ;; In Emacs 29 there is a binding for `repeat-mode' which let you
+  ;; In Emacs 29 there is a binding for `repeat-mode' which lets you
   ;; repeat C-x C-j just by following it up with j.  For me, this is a
   ;; problem as j calls `dired-goto-file', which I often use.
   (define-key dired-jump-map (kbd "j") nil))
