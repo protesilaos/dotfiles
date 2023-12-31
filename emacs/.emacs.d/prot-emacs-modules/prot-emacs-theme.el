2b605c20d213815421b5ee2314ac157d1b84fd91
Protesilaos Stavrou <info@protesilaos.com>
2023-12-31 06:11:26 +0200

    emacs: make pulsar reveal and recentre entry in next-error-hook
    This also works for the occur buffer's n and p motions.

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-theme.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-theme.el
@@ -26,6 +26,8 @@
   ;; There are convenience functions/commands which pulse the line using
   ;; a specific colour: `pulsar-pulse-line-red' is one of them.
   (add-hook 'next-error-hook #'pulsar-pulse-line-red)
+  (add-hook 'next-error-hook #'pulsar-recenter-top)
+  (add-hook 'next-error-hook #'pulsar-reveal-entry)
 
   (add-hook 'minibuffer-setup-hook #'pulsar-pulse-line-red)
 
