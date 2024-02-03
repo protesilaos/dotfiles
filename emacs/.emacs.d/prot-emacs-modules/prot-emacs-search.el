fdde897d167438366c9be2a1e2fcb07b0970e90a
Protesilaos Stavrou <info@protesilaos.com>
2024-02-03 09:33:33 +0200

    emacs: define alternative binding for prot-search-outline

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-search.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-search.el
@@ -54,6 +54,7 @@
     "M-s t" #'prot-search-occur-todo-keywords
     "M-s M-t" #'prot-search-grep-todo-keywords ; With C-u it runs `prot-search-git-grep-todo-keywords'
     "M-s M-T" #'prot-search-git-grep-todo-keywords
+    "M-s s" #'prot-search-outline
     "M-s M-s" #'prot-search-outline
     "M-s M-o" #'prot-search-occur-outline
     "M-s M-u" #'prot-search-occur-browse-url)
