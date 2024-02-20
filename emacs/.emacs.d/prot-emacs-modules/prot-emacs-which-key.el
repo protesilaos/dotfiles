66ae5931fc74b2c357ac5cc76c12b0b8c4e5f9c2
Protesilaos Stavrou <info@protesilaos.com>
2024-02-20 15:04:31 +0200

    emacs: increase the which-key-idle-delay

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-which-key.el
@@ -4,7 +4,7 @@
   (setq which-key-separator "  ")
   (setq which-key-prefix-prefix "... ")
   (setq which-key-max-display-columns 3)
-  (setq which-key-idle-delay 0.5)
+  (setq which-key-idle-delay 1.5)
   (setq which-key-idle-secondary-delay 0.25)
   (setq which-key-add-column-padding 1)
   (setq which-key-max-description-length 40)
