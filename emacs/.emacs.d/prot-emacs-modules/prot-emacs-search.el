fc70fe18e967901fbfedc2e9ca5e4867d40eff92
Protesilaos Stavrou <info@protesilaos.com>
2024-04-18 14:49:50 +0300

    emacs: convert all search settings to use-package
    As part of the process to make my Emacs setup consistent with what the
    community considers the de facto standard.

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-search.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-search.el
@@ -1,35 +1,78 @@
 ;;; Isearch, occur, grep, and extras (prot-search.el)
-(prot-emacs-package isearch
-  (:delay 5)
+(use-package isearch
+  :ensure nil
+  :demand t
+  :config
   (setq search-whitespace-regexp ".*?" ; one `setq' here to make it obvious they are a bundle
         isearch-lax-whitespace t
-        isearch-regexp-lax-whitespace nil)
+        isearch-regexp-lax-whitespace nil))
 
+(use-package isearch
+  :ensure nil
+  :demand t
+  :config
   (setq search-highlight t)
   (setq isearch-lazy-highlight t)
   (setq lazy-highlight-initial-delay 0.5)
-  (setq lazy-highlight-no-delay-length 4)
+  (setq lazy-highlight-no-delay-length 4))
 
+(use-package isearch
+  :ensure nil
+  :demand t
+  :config
   (setq isearch-lazy-count t)
   (setq lazy-count-prefix-format "(%s/%s) ")
-  (setq lazy-count-suffix-format nil)
+  (setq lazy-count-suffix-format nil))
 
-  (setq isearch-repeat-on-direction-change t)
+(use-package isearch
+  :ensure nil
+  :demand t
+  :config
   (setq isearch-wrap-pause t) ; `no-ding' makes keyboard macros never quit
+  (setq isearch-repeat-on-direction-change t))
 
+(use-package isearch
+  :ensure nil
+  :demand t
+  :config
   (setq list-matching-lines-jump-to-current-line nil) ; do not jump to current line in `*occur*' buffers
-  (add-hook 'occur-mode-hook #'hl-line-mode)
   (add-hook 'occur-mode-hook #'prot-common-truncate-lines-silently) ; from `prot-common.el'
+  (add-hook 'occur-mode-hook #'hl-line-mode))
 
-  (define-key minibuffer-local-isearch-map (kbd "M-/") #'isearch-complete-edit)
-  (define-key occur-mode-map (kbd "t") #'toggle-truncate-lines)
+(use-package isearch
+  :ensure nil
+  :demand t
+  :bind
+  ( :map minibuffer-local-isearch-map
+    ("M-/" . isearch-complete-edit)
+    :map occur-mode-map
+    ("t" . toggle-truncate-lines)
+    :map isearch-mode-map
+    ("C-g" . isearch-cancel) ; instead of `isearch-abort'
+    ("M-/" . isearch-complete)))
 
-  (prot-emacs-keybind isearch-mode-map
-    "C-g" #'isearch-cancel ; instead of `isearch-abort'
-    "M-/" #'isearch-complete))
-
-(prot-emacs-package prot-search
-  (:delay 5)
+(use-package prot-search
+  :ensure nil
+  :bind
+  ( :map global-map
+    ("M-s M-%" . prot-search-replace-markup) ; see `prot-search-markup-replacements'
+    ("M-s M-<" . prot-search-isearch-beginning-of-buffer)
+    ("M-s M->" . prot-search-isearch-end-of-buffer)
+    ("M-s g" . prot-search-grep)
+    ("M-s u" . prot-search-occur-urls)
+    ("M-s t" . prot-search-occur-todo-keywords)
+    ("M-s M-t" . prot-search-grep-todo-keywords) ; With C-u it runs `prot-search-git-grep-todo-keywords'
+    ("M-s M-T" . prot-search-git-grep-todo-keywords)
+    ("M-s s" . prot-search-outline)
+    ("M-s M-s" . prot-search-outline)
+    ("M-s M-o" . prot-search-occur-outline)
+    ("M-s M-u" . prot-search-occur-browse-url)
+    :map isearch-mode-map
+    ("<up>" . prot-search-isearch-repeat-backward)
+    ("<down>" . prot-search-isearch-repeat-forward)
+    ("<backspace>" . prot-search-isearch-abort-dwim)
+    ("<C-return>" . prot-search-isearch-other-end))
+  :config
   (setq prot-search-outline-regexp-alist
         '((emacs-lisp-mode . "^\\((\\|;;;+ \\)")
           (org-mode . "^\\(\\*+ +\\|#\\+[Tt][Ii][Tt][Ll][Ee]:\\)")
@@ -43,36 +86,28 @@
 
   (with-eval-after-load 'pulsar
     (add-hook 'prot-search-outline-hook #'pulsar-recenter-center)
-    (add-hook 'prot-search-outline-hook #'pulsar-reveal-entry))
-
-  (prot-emacs-keybind global-map
-    "M-s M-%" #'prot-search-replace-markup ; see `prot-search-markup-replacements'
-    "M-s M-<" #'prot-search-isearch-beginning-of-buffer
-    "M-s M->" #'prot-search-isearch-end-of-buffer
-    "M-s g" #'prot-search-grep
-    "M-s u" #'prot-search-occur-urls
-    "M-s t" #'prot-search-occur-todo-keywords
-    "M-s M-t" #'prot-search-grep-todo-keywords ; With C-u it runs `prot-search-git-grep-todo-keywords'
-    "M-s M-T" #'prot-search-git-grep-todo-keywords
-    "M-s s" #'prot-search-outline
-    "M-s M-s" #'prot-search-outline
-    "M-s M-o" #'prot-search-occur-outline
-    "M-s M-u" #'prot-search-occur-browse-url)
-  (prot-emacs-keybind isearch-mode-map
-    "<up>" #'prot-search-isearch-repeat-backward
-    "<down>" #'prot-search-isearch-repeat-forward
-    "<backspace>" #'prot-search-isearch-abort-dwim
-    "<C-return>" #'prot-search-isearch-other-end))
+    (add-hook 'prot-search-outline-hook #'pulsar-reveal-entry)))
 
 ;;; grep and xref
-(prot-emacs-configure
-  (:delay 5)
-  (setq reb-re-syntax 'read)
+(use-package re-builder
+  :ensure nil
+  :commands (re-builder regexp-builder)
+  :config
+  (setq reb-re-syntax 'read))
 
+(use-package xref
+  :ensure nil
+  :commands (xref-find-definitions xref-go-back)
+  :config
   ;; All those have been changed for Emacs 28
   (setq xref-show-definitions-function #'xref-show-definitions-completing-read) ; for M-.
   (setq xref-show-xrefs-function #'xref-show-definitions-buffer) ; for grep and the like
-  (setq xref-file-name-display 'project-relative)
+  (setq xref-file-name-display 'project-relative))
+
+(use-package grep
+  :ensure nil
+  :commands (grep lgrep rgrep)
+  :config
   (setq grep-save-buffers nil)
   (setq grep-use-headings t) ; Emacs 30
 
@@ -86,14 +121,16 @@
     (setq xref-search-program (if rgp 'ripgrep 'grep))))
 
 ;;; wgrep (writable grep)
-(prot-emacs-package wgrep
-  (:install t)
-  (:delay 5)
+(use-package wgrep
+  :ensure t
+  :after grep
+  :bind
+  ( :map grep-mode-map
+    ("e" . wgrep-change-to-wgrep-mode)
+    ("C-x C-q" . wgrep-change-to-wgrep-mode)
+    ("C-c C-c" . wgrep-finish-edit))
+  :config
   (setq wgrep-auto-save-buffer t)
-  (setq wgrep-change-readonly-file t)
-  (prot-emacs-keybind grep-mode-map
-    "e" #'wgrep-change-to-wgrep-mode
-    "C-x C-q" #'wgrep-change-to-wgrep-mode
-    "C-c C-c" #'wgrep-finish-edit))
+  (setq wgrep-change-readonly-file t))
 
 (provide 'prot-emacs-search)
