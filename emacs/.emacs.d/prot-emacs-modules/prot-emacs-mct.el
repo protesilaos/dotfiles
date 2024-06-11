ce9c480a077bf0ceee06175edf8ecdce9ddb2b0a
Protesilaos Stavrou <info@protesilaos.com>
2024-06-11 11:22:41 +0300

    emacs: add mct key for deleting directories in one go

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-mct.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-mct.el
@@ -30,6 +30,25 @@
   (setq mct-remove-shadowed-file-names t)
   (setq mct-completion-window-size (cons #'mct-frame-height-third 1))
   (setq mct-live-completion 'visible)
-  (setq completions-sort #'mct-sort-multi-category))
+  (setq completions-sort #'mct-sort-multi-category)
+
+  ;; Adaptation of `icomplete-fido-backward-updir'.
+  (defun prot/mct-backward-updir ()
+    "Delete char before point or go up a directory."
+    (interactive nil mct-mode)
+    (cond
+     ((and (eq (char-before) ?/)
+           (eq (mct--completion-category) 'file))
+      (when (string-equal (minibuffer-contents) "~/")
+        (delete-minibuffer-contents)
+        (insert (expand-file-name "~/"))
+        (goto-char (line-end-position)))
+      (save-excursion
+        (goto-char (1- (point)))
+        (when (search-backward "/" (minibuffer-prompt-end) t)
+          (delete-region (1+ (point)) (point-max)))))
+     (t (call-interactively 'backward-delete-char))))
+
+  (define-key minibuffer-local-filename-completion-map (kbd "DEL") #'prot/mct-backward-updir))
 
 (provide 'prot-emacs-mct)
