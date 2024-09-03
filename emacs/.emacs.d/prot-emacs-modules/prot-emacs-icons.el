c2879109bf9f8240cc1466a73ff3a8c1e2a56768
Protesilaos Stavrou <info@protesilaos.com>
2024-09-03 09:55:34 +0300

    emacs: tweak how the nerd-icons-completion is loaded

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-icons.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-icons.el
@@ -4,7 +4,14 @@
 
 (use-package nerd-icons-completion
   :ensure t
-  :hook (marginalia-mode . nerd-icons-completion-marginalia-setup))
+  :after marginalia
+  ;; FIXME 2024-09-01: For some reason this stopped working because it
+  ;; macroexpands to `marginalia-mode' instead of
+  ;; `marginalia-mode-hook'.  What is more puzzling is that this does
+  ;; not happen in the next :hook...
+  ;; :hook (marginalia-mode . nerd-icons-completion-marginalia-setup))
+  :config
+  (add-hook 'marginalia-mode-hook #'nerd-icons-completion-marginalia-setup))
 
 (use-package nerd-icons-corfu
   :ensure t
