bbfdac8e6c300b2ad0b94ea0045c38565a374668
Protesilaos Stavrou <info@protesilaos.com>
2024-09-15 09:39:06 +0300

    emacs: tweak the initial-scratch-message

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -103,7 +103,9 @@ before all other modules of my setup."
 
 (setq initial-buffer-choice t)
 (setq initial-major-mode 'lisp-interaction-mode)
-(setq initial-scratch-message nil)
+(setq initial-scratch-message
+      (format ";; Use %s to evaluate and print results\n\n"
+              (substitute-command-keys "\\<lisp-interaction-mode-map>\\[eval-print-last-sexp]")))
 
 (mapc
  (lambda (string)
