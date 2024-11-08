e980f93c6216b12c68955d74720dd3ac46351b78
Protesilaos Stavrou <info@protesilaos.com>
2024-11-08 21:41:47 +0200

    Revert "Update email setup to include a gmail account (FOR TESTING)"
    This reverts commit 4503ebfedd2b3cc99249589ff8bfd924be8e40b1.

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-notmuch.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-notmuch.el
@@ -13,16 +13,14 @@
   :defer t
   :config
   (let ((prv (prot-common-auth-get-field "prv" :user))
-        (gml (prot-common-auth-get-field "gml" :user))
         (pub (prot-common-auth-get-field "pub" :user))
         (inf (prot-common-auth-get-field "inf" :user)))
     (setq notmuch-identities
           (mapcar (lambda (str)
                     (format "%s <%s>" user-full-name str))
-                  (list prv gml pub inf))
+                  (list prv pub inf))
           notmuch-fcc-dirs
           `((,prv . "prv/Sent")
-            (,gml . "\"gml/[Gmail]/Sent Mail\"")
             (,inf . "inf/Sent")
             (,pub . "pub/Sent")))))
 
