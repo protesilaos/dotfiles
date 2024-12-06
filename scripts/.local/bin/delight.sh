protesilaos/dotfiles

commit e94e0e2ba0a9d203e20a86c0ac627d8f848c1d9b

BREAKING rename all shell scripts to .sh and put them in ~/.local/bin

scripts/.local/bin/delight.sh
@@ -92,7 +92,7 @@ _xterm ()
 
     # Another one of my scripts for live recolouring running terminal
     # emulators, by using escape sequences.
-    repaint_terminals &
+    repaint_terminals.sh &
 }
 
 # TODO 2021-08-27: dunst

xorg-twm/.config/bspwm/bspwmrc
@@ -62,14 +62,14 @@ _check bspwm_external_rules && \
         "$(command -v bspwm_external_rules)"
 
 #### Colours
-_check bspwm_conf_colors && bspwm_conf_colors
+_check bspwm_conf_colors.sh && bspwm_conf_colors.sh
 
 #### Autostart
 
 # Common settings used by my BSPWM and Herbstluftwm setups
 # (collectively referred to as "Xorg Tiling Window Managers").  I
 # configure those window managers to be similar.
-_check xtwm_common && xtwm_common
+_check xtwm_common.sh && xtwm_common.sh
 
 # Add padding equal to the panel height to fix an issue where there is
 # an overlap between panel and windows.

xorg-twm/.config/sxhkd/sxhkdrc_bspwm
@@ -47,10 +47,10 @@ mod4 + {Left,Down,Up,Right}
 #
 # bspwm_smart_move is part of my dotfiles (link in the description).
 mod4 + shift + {h,j,k,l}
-    bspwm_smart_move {west,south,north,east}
+    bspwm_smart_move.sh {west,south,north,east}
 
 mod4 + shift + {Left,Down,Up,Right}
-    bspwm_smart_move {west,south,north,east}
+    bspwm_smart_move.sh {west,south,north,east}
 
 # Dynamic desktop behaviour:  this invokes a script of mine that
 # implements dynamic desktops (see dotfiles).  If the given desktop
@@ -80,7 +80,7 @@ mod4 + shift + {Left,Down,Up,Right}
 # which replaces the "send" commands noted above with their
 # "send+follow" equivalents.
 mod4 + {_,shift + ,ctrl + shift +,ctrl +}{0-9}
-    ;bspwm_dynamic_desktops {--df,--ns,--nm,--da} {0-9}
+    ;bspwm_dynamic_desktops.sh {--df,--ns,--nm,--da} {0-9}
 
 # mod4 + {_,shift + }{1-9}
 #     bspc {desktop -f,node -d} {1-9}
@@ -183,11 +183,11 @@ mod4 + {_,shift + }r
 # increasing or decreasing past either of those limits, we move to the
 # other end: 0 5 ... 30 0 5.
 mod4 + bracket{left,right}
-    bspwm_adjust_gaps {-,+}5
+    bspwm_adjust_gaps.sh {-,+}5
 
 # Like the above, but for border width.
 mod4 + shift + bracket{left,right}
-    bspwm_adjust_borders {-,+}5
+    bspwm_adjust_borders.sh {-,+}5
 
 #### Advanced operations (receptacles and scripted actions)
 
@@ -209,34 +209,34 @@ mod4 + n ; r ; r
 # Insert the marked window (or focused if no marks) in the first
 # matching receptacle (or preselection if no receptacles).
 mod4 + n ; r ; i
-    bspwm_smart_presel
+    bspwm_smart_presel.sh
 
 # Kill/remove the first matching receptacle from the current desktop.
 # With shift, kill them all at once.
 mod4 + n ; r ; {_, shift + }k
     { \
     bspc node 'any.leaf.!window.local' -k, \
-    bspwm_multifaceted_operation --kill-all-receptacles \
+    bspwm_multifaceted_operation.sh --kill-all-receptacles \
     }
 
 # Summon all marked windows across all desktops to the current desktop.
 mod4 + w ; m
-    bspwm_multifaceted_operation --summon-all-marked
+    bspwm_multifaceted_operation.sh --summon-all-marked
 
 # Bring the windows of the given desktop to the current one.  With
 # shift, send the current window to the given one.
 mod4 + w ; mod4 + {_, shift + }{0-9}
-    bspwm_multifaceted_operation {--bring-desktop-here,--send-desktop-there} {0-9}
+    bspwm_multifaceted_operation.sh {--bring-desktop-here,--send-desktop-there} {0-9}
 
 # Close {or kill} all windows in the present desktop except the focused
 # or any marked ones.
 mod4 + w ; mod4 + {_,shift + }q
-    bspwm_multifaceted_operation --{close,kill}-all-local-non-focused
+    bspwm_multifaceted_operation.sh --{close,kill}-all-local-non-focused
 
 # Assign the locked flag to all windows in the current desktop.  With
 # shift, remove the locked flag from all.
 mod4 + w ; {_, shift + }l
-    bspwm_multifaceted_operation {--lock-all-local,--unlock-all-local}
+    bspwm_multifaceted_operation.sh {--lock-all-local,--unlock-all-local}
 
 
 #### Continuous input mode
@@ -252,7 +252,7 @@ mod4 + w ; {_, shift + }l
 
 # Resize node in the given direction.
 mod4 + c : {h,j,k,l}
-    bspwm_resize {west,south,north,east} 50
+    bspwm_resize.sh {west,south,north,east} 50
 
 # Mode for dynamic gaps.
 mod4 + c : bracket{left,right}

commit abef0172d70221fb5d8e6db41305e0b074393dbd

BREAKING fix some shell script names caused by commit e94e0e2b

scripts/.local/bin/delight.sh
@@ -368,13 +368,13 @@ case "$DESKTOP_SESSION"
 in
     bspwm)
         wm_executable=bspwm
-        conf_colors=bspwm_conf_colors
+        conf_colors=bspwm_conf_colors.sh
 
         _bspwm_or_hlwm
         ;;
     herbstluftwm)
         wm_executable=herbstluftwm
-        conf_colors=herbstluftwm_conf_colors
+        conf_colors=herbstluftwm_conf_colors.sh
 
         _bspwm_or_hlwm
         ;;

scripts/.local/bin/poweroptionsmenu.sh
@@ -27,7 +27,7 @@ _pkill() {
 }
 
 _kill_processes() {
-    _pkill bspwm_external_rules
+    _pkill bspwm_external_rules.sh
     _pkill sxhkd
     _pkill polybar
     _pkill feh

xorg-twm/.config/bspwm/bspwmrc
@@ -57,11 +57,11 @@ _check() {
 
 # # Determine XrandR settings.  Whether an external display is connected
 # # to the laptop or not.
-# _check bspwm_conf_xrandr && bspwm_conf_xrandr
+# _check bspwm_conf_xrandr.sh && bspwm_conf_xrandr.sh
 
 # Define desktop[s] [per monitor].  Contingent on the above.  Also note
 # that this leverages my `bspwm_dynamic_desktops'.
-_check bspwm_conf_desktops && bspwm_conf_desktops
+_check bspwm_conf_desktops.sh && bspwm_conf_desktops.sh
 
 #### Window management
 _bc automatic_scheme spiral
@@ -79,9 +79,9 @@ _bc pointer_action1 move
 _bc pointer_action2 resize_side
 _bc ignore_ewmh_focus false
 
-_check bspwm_external_rules && \
+_check bspwm_external_rules.sh && \
     _bc external_rules_command \
-        "$(command -v bspwm_external_rules)"
+        "$(command -v bspwm_external_rules.sh)"
 
 #### Colours
 _check bspwm_conf_colors.sh && bspwm_conf_colors.sh

xorg-twm/.config/polybar/config.ini
@@ -58,7 +58,7 @@ tray-padding = 5
 
 format-margin = 0
 
-modules-left = bspwm bspwm_panel_monocle title
+modules-left = bspwm bspwm_panel_monocle.sh title
 modules-center =
 modules-right = xkeyboard alsa date
 wm-restack = bspwm

xorg-twm/.config/sxhkd/sxhkdrc_bspwm
@@ -45,7 +45,7 @@ mod4 + {Left,Down,Up,Right}
 # spread out the nodes (see the key chord further down).  You may also
 # --rotate the tree.
 #
-# bspwm_smart_move is part of my dotfiles (link in the description).
+# bspwm_smart_move.sh is part of my dotfiles (link in the description).
 mod4 + shift + {h,j,k,l}
     bspwm_smart_move.sh {west,south,north,east}
 
@@ -65,7 +65,7 @@ mod4 + shift + {Left,Down,Up,Right}
 # unfocused monitor, without focusing it (focus stays on the current
 # desktop/monitor).  It also inserts a receptacle, meaning that the next
 # created window will go there without calling focus to itself (see my
-# bspwm_external_rules).
+# bspwm_external_rules.sh).
 #
 #
 # Commands for receptacles are defined in the "advanced operations"
@@ -87,7 +87,7 @@ mod4 + {_,shift + ,ctrl + shift +,ctrl +}{0-9}
 
 ## Use this for "send+follow" instead of just "send"
 # mod4 + {_,shift + ,ctrl + shift +,ctrl +}{0-9}
-#   ;bspwm_dynamic_desktops {--df,--nsf,--nmf,--da} {0-9}
+#   ;bspwm_dynamic_desktops.sh {--df,--nsf,--nmf,--da} {0-9}
 
 # With `alt` focus {next,previous} window in the current desktop.  With
 # `mod4` focus {next,previous} desktop in the current monitor.
@@ -114,10 +114,10 @@ mod4 + o
 # windows because: (i) I do not use them and if I do I do not want to
 # change their dimensions, and (ii) to keep the script concise.
 mod4 + ctrl + {h,j,k,l}
-    bspwm_resize {west,south,north,east} 50
+    bspwm_resize.sh {west,south,north,east} 50
 
 mod4 + ctrl + {Left,Down,Up,Right}
-    bspwm_resize {west,south,north,east} 50
+    bspwm_resize.sh {west,south,north,east} 50
 
 # Preselect the direction or insert again to cancel the preselection.
 # This enters the manual tiling mode that splits the currently focused
@@ -192,13 +192,13 @@ mod4 + shift + bracket{left,right}
 #### Advanced operations (receptacles and scripted actions)
 
 # NOTE The behaviour of preselections and receptacles is regulated by my
-# bspwm_external_rules command (which is called from within bspwmrc).
+# bspwm_external_rules.sh command (which is called from within bspwmrc).
 
 # Insert receptacle following the tiling scheme or preselection.
 # Receptacles are empty frames that function as placeholders for
 # windows.
 #
-# Whereas bspwm_smart_move (defined above) will automatically insert
+# Whereas bspwm_smart_move.sh (defined above) will automatically insert
 # a receptacle in the given direction by splitting the entire viewport.
 #
 # The mod4 + n prefix is a mnemonic for "node" and then r is for
@@ -252,7 +252,7 @@ mod4 + w ; {_, shift + }l
 
 # Resize node in the given direction.
 mod4 + c : {h,j,k,l}
-    bspwm_resize.sh {west,south,north,east} 50
+    bspwm_resize.sh.sh {west,south,north,east} 50
 
 # Mode for dynamic gaps.
 mod4 + c : bracket{left,right}

xorg-twm/.local/bin/bspwm_conf_desktops.sh
@@ -20,7 +20,7 @@
 if [ "$(bspc query -M | wc -l)" -eq 2 ]; then
 	# Set the workspaces per monitor.  We only define one per monitor,
 	# because of another script I have that implements dynamic desktops.
-	# For the XrandR settings see my `bspwm_conf_xrandr'.
+	# For the XrandR settings see my `bspwm_conf_xrandr.sh'.
 	bspc monitor LVDS-1 -d 1
 	bspc monitor VGA-1 -d 8
 

xorg-twm/.local/bin/bspwm_dynamic_desktops.sh
@@ -42,7 +42,7 @@
 # monitor in bspwmrc (empty desktops are removed upon invoking one of
 # this script's commands).
 #
-# `bspwm_dynamic_desktops' is part of my dotfiles (which also offer a
+# `bspwm_dynamic_desktops.sh' is part of my dotfiles (which also offer a
 # complete implementation): https://github.com/protesilaos/dotfiles
 
 

xorg-twm/.local/bin/bspwm_external_rules.sh
@@ -68,6 +68,9 @@ window_title="$(xwininfo -id "$window_id" | sed ' /^xwininfo/!d ; s,.*"\(.*\)".*
 # NOTE 2021-09-06: I don't necessarily use those, but it is good to have
 # this here.
 case "$window_class" in
+    [Ee]macs)
+        echo "state=tiled"
+        ;;
 	[Ee]o[mg]|[Ff]eh|[Rr]istretto|[Ss]xiv|my_float_window)
 		echo "state=floating"
 		echo "center=on"

xorg-twm/.local/bin/bspwm_multifaceted_operation.sh
@@ -55,6 +55,6 @@ case "$1" in
 		;;
 	--ds|--send-desktop-there)
 		# NOTE another script of mine (see dotfiles).
-		bspwm_dynamic_desktops --send-all-to-desktop "$2"
+		bspwm_dynamic_desktops.sh --send-all-to-desktop "$2"
 		;;
 esac

xorg-twm/.local/bin/bspwm_resize.sh
@@ -10,7 +10,7 @@
 # following sxhkdrc config:
 #
 # super + ctrl + {Left,Down,Up,Right}
-#     bspwm_resize {west,south,north,east} 50
+#     bspwm_resize.sh {west,south,north,east} 50
 
 size=${2:-'10'}
 direction=$1

xorg-twm/.local/bin/xtwm_focus_mode.sh
@@ -18,6 +18,6 @@
 
 case "$DESKTOP_SESSION"
 in
-    bspwm) bspwm_focus_mode ;;
-    herbstluftwm) herbstluftwm_focus_mode ;;
+    bspwm) bspwm_focus_mode.sh ;;
+    herbstluftwm) herbstluftwm_focus_mode.sh ;;
 esac