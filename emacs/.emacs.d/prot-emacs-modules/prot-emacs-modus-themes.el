f43b7fd8cba24aa69ec1a2c6053060a49ef25cc3
Protesilaos Stavrou <info@protesilaos.com>
2025-03-08 10:15:09 +0200

    emacs: tweak modus-themes configuration (experimental)

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-modus-themes.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-modus-themes.el
@@ -18,15 +18,20 @@
         modus-themes-mixed-fonts t
         modus-themes-variable-pitch-ui t
         modus-themes-italic-constructs t
-        modus-themes-bold-constructs nil
-        modus-themes-completions '((t . (extrabold)))
-        modus-themes-prompts '(extrabold)
+        modus-themes-bold-constructs t
+        modus-themes-completions '((t . (bold)))
+        modus-themes-prompts '(bold)
         modus-themes-headings
         '((agenda-structure . (variable-pitch light 2.2))
           (agenda-date . (variable-pitch regular 1.3))
           (t . (regular 1.15))))
 
-  (setq modus-themes-common-palette-overrides nil)
+  ;; (setq modus-themes-common-palette-overrides nil)
+  (setq modus-themes-common-palette-overrides
+        '((cursor cyan-intense)
+          (comment magenta-faint)
+          (bg-paren-match bg-magenta-subtle)
+          (fg-paren-match magenta)))
 
   (if (prot-emacs-theme-environment-dark-p)
       (modus-themes-load-theme (cadr modus-themes-to-toggle))
