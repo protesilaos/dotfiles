abde635eecf18c8c2c760329383ff4125ee2d45a
Protesilaos Stavrou <info@protesilaos.com>
2025-03-15 08:24:12 +0200

    emacs: add my denote-sequence package

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-langs.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-langs.el
@@ -368,6 +368,31 @@
               "~/Documents/books/"
               "~/Documents/denote-test-silo/")))
 
+;;;; Denote Sequence notes or folgezettel (denote-sequence)
+
+(use-package denote-sequence
+  :ensure t
+  :bind
+  ( :map global-map
+    ;; Here we make "C-c n s" a prefix for all "[n]otes with [s]equence".
+    ;; This is just for demonstration purposes: use the key bindings
+    ;; that work for you.  Also check the commands:
+    ;;
+    ;; - `denote-sequence-new-parent'
+    ;; - `denote-sequence-new-sibling'
+    ;; - `denote-sequence-new-child'
+    ;; - `denote-sequence-new-child-of-current'
+    ;; - `denote-sequence-new-sibling-of-current'
+    ("C-c n s s" . denote-sequence)
+    ("C-c n s f" . denote-sequence-find)
+    ("C-c n s l" . denote-sequence-link)
+    ("C-c n s d" . denote-sequence-dired)
+    ("C-c n s r" . denote-sequence-reparent)
+    ("C-c n s c" . denote-sequence-convert))
+  :config
+  ;; The default sequence scheme is `numeric'.
+  (setq denote-sequence-scheme 'alphanumeric))
+
 ;;; Custom extensions for "focus mode" (logos.el)
 ;; Read the manual: <https://protesilaos.com/emacs/logos>.
 (use-package olivetti
