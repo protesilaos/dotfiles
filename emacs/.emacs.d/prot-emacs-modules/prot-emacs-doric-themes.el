7994421309481147d3f76656c46a36c83e930fdb
Protesilaos Stavrou <info@protesilaos.com>
2025-10-27 19:18:34 +0200

    emacs: add configuration for testing the doric-themes

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-doric-themes.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-doric-themes.el
@@ -16,4 +16,23 @@
    ("C-<f5>" . doric-themes-select)
    ("M-<f5>" . doric-themes-rotate)))
 
+;; For testing purposes
+(prot-emacs-comment
+  (:eval nil)
+  (add-to-list 'load-path "/home/prot/Git/Projects/doric-themes/")
+
+  (require 'doric-themes)
+
+  (setq doric-themes-to-toggle '(doric-light doric-dark))
+  (setq doric-themes-to-rotate doric-themes-collection)
+  (doric-themes-load-random
+   (if (prot-emacs-theme-environment-dark-p)
+       'dark
+     'light))
+
+  (prot-emacs-keybind global-map
+    "<f5>" #'doric-themes-toggle
+    "C-<f5>" #'doric-themes-select
+    "M-<f5>" #'doric-themes-rotate))
+
 (provide 'prot-emacs-doric-themes)
