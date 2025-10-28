641cc998d43f025b08aae9d5660c623ef506f55a
Protesilaos Stavrou <info@protesilaos.com>
2025-10-28 09:49:29 +0200

    emacs: tweak how the nerd icons are loaded

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-icons.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-icons.el
@@ -1,11 +1,12 @@
 ;;; Icons
 (use-package nerd-icons
-  :ensure t)
+  :ensure t
+  :defer 1)
 
 (use-package nerd-icons-completion
   :ensure t
   :if (display-graphic-p)
-  :after marginalia
+  :after (nerd-icons marginalia)
   ;; FIXME 2024-09-01: For some reason this stopped working because it
   ;; macroexpands to `marginalia-mode' instead of
   ;; `marginalia-mode-hook'.  What is more puzzling is that this does
@@ -23,27 +24,28 @@
 (use-package nerd-icons-corfu
   :ensure t
   :if (display-graphic-p)
-  :after corfu
+  :after (nerd-icons corfu)
   :config
   (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
 (use-package nerd-icons-dired
   :ensure t
   :if (display-graphic-p)
+  :after nerd-icons
   :hook
   (dired-mode . nerd-icons-dired-mode))
 
 (use-package nerd-icons-xref
   :ensure t
   :if (display-graphic-p)
-  :after xref
+  :after (nerd-icons xref)
   :config
   (nerd-icons-xref-mode 1))
 
 (use-package nerd-icons-grep
   :ensure t
   :if (display-graphic-p)
-  :after grep
+  :after (nerd-icons grep)
   :config
   (when grep-use-headings
     (nerd-icons-grep-mode 1)))
