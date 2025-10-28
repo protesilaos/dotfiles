4879ca1dd07f77f952cef26488f0ed748c095957
Protesilaos Stavrou <info@protesilaos.com>
2025-10-28 20:29:30 +0200

    emacs: update the code used to test modus-themes, ef-themes, standard-themes

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-ef-themes.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-ef-themes.el
@@ -37,42 +37,23 @@
 
 (prot-emacs-comment
   (:eval nil)
-  (add-to-list 'load-path "/home/prot/Git/Projects/modus-themes/")
-  (add-to-list 'load-path "/home/prot/Git/Projects/ef-themes/")
+  (use-package modus-themes
+    :ensure nil
+    :load-path "/home/prot/Git/Projects/modus-themes/")
 
-  (require 'modus-themes)
-  (require 'ef-themes)
+  (use-package ef-themes
+    :ensure nil
+    :load-path "/home/prot/Git/Projects/ef-themes/"
+    :init
+    ;; (modus-themes-include-derivatives-mode 1)
+    (ef-themes-take-over-modus-themes-mode 1)
+    :bind
+    (("<f5>" . modus-themes-rotate)
+     ("C-<f5>" . modus-themes-select)
+     ("M-<f5>" . modus-themes-load-random))
+    :config
+    (if (prot-emacs-theme-environment-dark-p)
+        (modus-themes-load-random 'dark)
+      (modus-themes-load-random 'light))))
 
-  (ef-themes-take-over-modus-themes-mode 1)
-
-  (prot-emacs-keybind global-map
-    "<f5>" #'modus-themes-rotate
-    "C-<f5>" #'modus-themes-select
-    "M-<f5>" #'modus-themes-load-random)
-
-  (setq modus-themes-variable-pitch-ui t
-        modus-themes-mixed-fonts t
-        modus-themes-to-rotate nil ; defaults to the return value of `modus-themes-get-themes'
-        modus-themes-headings ; read the manual's entry of the doc string
-        '((0 . (variable-pitch light 1.9))
-          (1 . (variable-pitch light 1.8))
-          (2 . (variable-pitch regular 1.7))
-          (3 . (variable-pitch regular 1.6))
-          (4 . (variable-pitch regular 1.5))
-          (5 . (variable-pitch 1.4)) ; absence of weight means `bold'
-          (6 . (variable-pitch 1.3))
-          (7 . (variable-pitch 1.2))
-          (agenda-date . (semilight 1.5))
-          (agenda-structure . (variable-pitch light 1.9))
-          (t . (variable-pitch 1.1))))
-
-  ;; The `ef-themes' provide lots of themes.  I want to pick one at
-  ;; random when I start Emacs: the `modus-themes-load-random' does just
-  ;; that (it can be called interactively as well).  I just check with
-  ;; my desktop environment to determine if the choice should be about
-  ;; a light or a dark theme.  Those functions are in my init.el.
-  (if (prot-emacs-theme-environment-dark-p)
-      (modus-themes-load-random 'dark)
-    (modus-themes-load-random 'light)))
-
-(provide 'prot-emacs-ef-themes)
+  (provide 'prot-emacs-ef-themes)
--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-modus-themes.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-modus-themes.el
@@ -55,68 +55,19 @@
 ;; NOTE: For testing purposes
 (prot-emacs-comment
   (:eval nil)
-  (progn
-    (mapc #'disable-theme custom-enabled-themes)
-
-    (add-to-list 'load-path "/home/prot/Git/Projects/modus-themes/")
-
-    (require 'modus-themes)
-
-    (setq modus-themes-custom-auto-reload nil
-          modus-themes-to-toggle '(modus-operandi modus-vivendi)
-          ;; modus-themes-to-toggle '(modus-operandi-tinted modus-vivendi-tinted)
-          ;; modus-themes-to-toggle '(modus-operandi-deuteranopia modus-vivendi-deuteranopia)
-          ;; modus-themes-to-toggle '(modus-operandi-tritanopia modus-vivendi-tritanopia)
-          modus-themes-mixed-fonts t
-          modus-themes-variable-pitch-ui nil
-          modus-themes-italic-constructs t
-          modus-themes-bold-constructs t
-          modus-themes-completions '((t . (extrabold)))
-          modus-themes-prompts nil
-          modus-themes-headings
-          '((agenda-structure . (variable-pitch light 2.2))
-            (agenda-date . (variable-pitch regular 1.3))
-            (t . (regular 1.15))))
-
-    ;; (setq modus-themes-common-palette-overrides nil)
-
-    ;; (setq modus-themes-common-palette-overrides
-    ;;       `((fringe unspecified)
-    ;;         ;; (bg-mode-line-active bg-lavender)
-    ;;         ;; (border-mode-line-active unspecified)
-    ;;         ;; (border-mode-line-inactive unspecified)
-    ;;         (bg-line-number-active bg-hl-line)
-    ;;         (bg-line-number-inactive unspecified)
-    ;;         (fg-line-number-active fg-main)
-    ;;         ;; ,@modus-themes-preset-overrides-warmer
-    ;;         ))
-
-    ;; ;; For testing purposes I only want the overrides for those two
-    ;; ;; Modus themes.  The rest have their own styles already.
-    ;;
-    ;; (let ((overrides '((cursor blue-intense)
-    ;;                    (keybind green-cooler)
-    ;;                    (comment red-faint)
-    ;;                    (bg-paren-match unspecified)
-    ;;                    (fg-paren-match magenta-intense)
-    ;;                    (underline-paren-match magenta-intense))))
-    ;;   (setq modus-operandi-palette-overrides overrides
-    ;;         modus-vivendi-palette-overrides overrides))
-
-
-    ;; ;; Make the active mode line have a pseudo 3D effect (this assumes
-    ;; ;; you are using the default mode line and not an extra package).
-    ;; (custom-set-faces
-    ;;  '(mode-line ((t :box (:style unspecified)))))
-
+  (use-package modus-themes
+    :ensure nil
+    :load-path "/home/prot/Git/Projects/modus-themes/"
+    :demand t
+    :init
+    (modus-themes-include-derivatives-mode 1)
+    :bind
+    (("<f5>" . modus-themes-rotate)
+     ("C-<f5>" . modus-themes-select)
+     ("M-<f5>" . modus-themes-load-random))
+    :config
     (if (prot-emacs-theme-environment-dark-p)
-        (modus-themes-load-theme (cadr modus-themes-to-toggle))
-      (modus-themes-load-theme (car modus-themes-to-toggle)))
-
-    ;; Also check `modus-themes-select'.  To list the palette's colours,
-    ;; use `modus-themes-list-colors', `modus-themes-list-colors-current'.
-    (define-key global-map (kbd "<f5>") #'modus-themes-toggle)
-    (define-key global-map (kbd "C-<f5>") #'modus-themes-select)
-    (define-key global-map (kbd "M-<f5>") #'modus-themes-rotate)))
+        (modus-themes-load-random 'dark)
+      (modus-themes-load-random 'light))))
 
 (provide 'prot-emacs-modus-themes)
