3170d1299f5d9acbd5be75405069d3a871329ac5
Protesilaos Stavrou <info@protesilaos.com>
2025-11-01 11:57:48 +0200

    emacs: BREAKING stop using use-package for consult

--- emacs/.emacs.d/prot-emacs-modules/prot-emacs-completion.el
+++ emacs/.emacs.d/prot-emacs-modules/prot-emacs-completion.el
@@ -326,24 +326,23 @@ Development continues on GitHub with GitLab as a mirror."))
 
 ;;; Enhanced minibuffer commands (consult.el)
 (when prot-emacs-completion-extras
-  (use-package consult
-    :ensure t
-    :hook (completion-list-mode . consult-preview-at-point-mode)
-    :bind
-    ( :map global-map
-      ("M-g M-g" . consult-goto-line)
-      ("M-s M-b" . consult-buffer)
-      ("M-s M-f" . consult-find)
-      ("M-s M-g" . consult-grep)
-      ("M-s M-h" . consult-history)
-      ("M-s M-i" . consult-imenu)
-      ("M-s M-l" . consult-line)
-      ("M-s M-m" . consult-mark)
-      ("M-s M-y" . consult-yank-pop)
-      ("M-s M-s" . consult-outline)
-      :map consult-narrow-map
-      ("?" . consult-narrow-help))
-    :config
+  (prot-emacs-configure
+    (prot-emacs-install consult)
+
+    (add-hook 'completion-list-mode-hook #'consult-preview-at-point-mode)
+
+    (prot-emacs-keybind global-map
+      "M-g M-g" #'consult-goto-line
+      "M-s M-b" #'consult-buffer
+      "M-s M-f" #'consult-find
+      "M-s M-g" #'consult-grep
+      "M-s M-h" #'consult-history
+      "M-s M-i" #'consult-imenu
+      "M-s M-l" #'consult-line
+      "M-s M-m" #'consult-mark
+      "M-s M-y" #'consult-yank-pop
+      "M-s M-s" #'consult-outline)
+
     (setq consult-line-numbers-widen t)
     ;; (setq completion-in-region-function #'consult-completion-in-region)
     (setq consult-async-min-input 3)
@@ -357,15 +356,13 @@ Development continues on GitHub with GitLab as a mirror."))
     (setq consult-preview-key 'any)
     (setq consult-project-function nil) ; always work from the current directory (use `cd' to switch directory)
 
-    (add-to-list 'consult-mode-histories '(vc-git-log-edit-mode . log-edit-comment-ring))
-
-    (require 'consult-imenu) ; the `imenu' extension is in its own file
-
-    (with-eval-after-load 'pulsar
-      ;; see my `pulsar' package: <https://protesilaos.com/emacs/pulsar>
-      (setq consult-after-jump-hook nil) ; reset it to avoid conflicts with my function
-      (dolist (fn '(pulsar-recenter-top pulsar-reveal-entry))
-        (add-hook 'consult-after-jump-hook fn)))))
+    ;; see my `pulsar' package: <https://protesilaos.com/emacs/pulsar>
+    (setq consult-after-jump-hook nil) ; reset it to avoid conflicts with my function
+    (prot-emacs-hook
+      consult-after-jump-hook
+      (pulsar-recenter-top pulsar-reveal-entry)
+      nil
+      pulsar)))
 
 ;;; Extended minibuffer actions and more (embark.el)
 (when prot-emacs-completion-extras
